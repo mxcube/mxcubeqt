559a47c969038996b0fb24010baa62ae938f8873
Marcus Oskarsson <oscarsso@linoscarsson.esrf.fr>
2013-05-15 15:09:05 +0200

    Added some code documentation.

--- HardwareObjects/XMLRPCServer.py
+++ HardwareObjects/XMLRPCServer.py
@@ -1,3 +1,16 @@
+"""
+XMLRPC-Server that makes it possbile to access core features of MXCuBE like
+the queue from external applications. The Server is implemented as a
+hardware object and is configured with an XML-file. See the example
+configuration XML below.
+
+<object class="XMLRPCServer" role="XMLRPCServer">
+  <port>
+    8000
+  </port>
+</object>
+"""
+
 import logging
 import gevent
 import queue_entry
@@ -11,7 +24,16 @@ from queue_entry import QueueEntryContainer
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 
-        
+__author__ = "Marcus Oskarsson, Matias Guijarro"
+__copyright__ = "Copyright 2012, ESRF"
+__credits__ = ["MxCuBE colaboration"]
+
+__version__ = ""
+__maintainer__ = "Marcus Oskarsson"
+__email__ = "marcus.oscarsson@esrf.fr"
+__status__ = "Draft"
+
+
 class XMLRPCServer(HardwareObject):
     def __init__(self, name):
         HardwareObject.__init__(self, name)
@@ -32,6 +54,24 @@ class XMLRPCServer(HardwareObject):
         
 
     def add_to_queue(self, json_task_node, set_on = True):
+        """
+        Adds the TaskNode objects contained in the json seralized
+        list of TaskNodes passed in <json_task_node>.
+        
+        The TaskNodes are marked as activated in the queue if <set_on>
+        is True and to inactivated if False.
+
+        :param json_task_node: TaskNode object to add to queue
+        :type parent: TaskNode
+
+        :param set_on: Mark TaskNode as activated if True and as inactivated
+                       if false.
+        :type set_on: Boolean
+
+        :returns: True on success otherwise False
+        :rtype: Boolean
+        """
+        
         # The exception is re raised so that it will
         # be sent to the client.
         try:
@@ -45,6 +85,12 @@ class XMLRPCServer(HardwareObject):
 
 
     def start_queue(self):
+        """
+        Starts the queue execution.
+
+        :returns: True on success otherwise False
+        :rtype: Boolean
+        """
         try:
             self.emit('start_queue')
         except Exception as ex:
@@ -55,6 +101,21 @@ class XMLRPCServer(HardwareObject):
 
 
     def log_message(self, message, level = 'info'):
+        """
+        Logs a message in the user_level_log of MxCuBE,
+        normally displayed at the bottom of the MxCuBE
+        window.
+
+        :param message: The message to log
+        :type parent: str
+
+        :param message: The log level, one of the strings:
+                        'info'. 'warning', 'error'
+        :type parent: str
+
+        :returns: True on success otherwise False
+        :rtype: Boolean
+        """ 
         status = True
         
         if level == 'info':
