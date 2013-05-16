3327785a1548eee5087bc706f15911d8692916e3
Marcus Oskarsson <oscarsso@linoscarsson.esrf.fr>
2013-05-16 11:46:44 +0200

    Added some code documentation.

--- HardwareObjects/QueueController.py
+++ HardwareObjects/QueueController.py
@@ -1,9 +1,54 @@
+"""
+QueueController, handles the execution of the MxCuBE queue. It is implemented
+as a hardware object and is configured by an XML file. See the example of the
+XML configuration below for more details.
+
+<object class = "QueueController" role = "QueueController">
+  <object href="/sc" role="sample_changer"/>    
+  <object href="/minidiff" role="diffractometer"/>
+  <object href="/energyscan" role="energy"/>
+  <object href="/mxlocal" role="beamline_configuration"/>
+  <object href="/data-analysis" role="data_analysis"/>
+  <object href="/mxcollect" role="collect"/>
+</object>
+
+The QueueController acts as both the controller of execution and as the root/
+container of the queue, note the inheritance from QueueEntryContainer. See the
+documentation for the queue_entry module for more information.
+"""
+
 import logging
 import gevent
 import queue_entry
 
+
 from HardwareRepository.BaseHardwareObjects import HardwareObject
 from queue_entry import QueueEntryContainer
+
+__author__ = "Marcus Oskarsson"
+__copyright__ = "Copyright 2012, ESRF"
+__credits__ = ["My great coleagues", "The MxCuBE colaboration"]
+
+__version__ = "0.1"
+__maintainer__ = "Marcus Oskarsson"
+__email__ = "marcus.oscarsson@esrf.fr"
+__status__ = "Beta"
+
+
+logger = logging.getLogger('queue_exec')
+try:
+    formatter = \
+              logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr = logging.FileHandler('/users/blissadm/log/queue_exec.log')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+except:
+    pass
+
+logger.setLevel(logging.INFO)
+logger = logging.getLogger('queue_exec').\
+         info("Module load, probably application start")
+
 
 class QueueController(HardwareObject, QueueEntryContainer):
     def __init__(self, name):
@@ -16,12 +61,42 @@ class QueueController(HardwareObject, QueueEntryContainer):
 
 
     def enqueue(self, queue_entry):
+        """
+        Method inherited from QueueEntryContainer, enqueues the QueueEntry
+        object <queue_entry>.
+
+        :param queue_entry: QueueEntry to add
+        :type queue_entry: QueueEntry
+
+        :returns: None
+        :rtype: NoneType
+        """
         QueueEntryContainer.enqueue(self, queue_entry)
         
 
     def execute(self):
+        """
+        Starts execution of the queue.
+        """
         self._root_task = gevent.spawn(self.__execute_task)
 
+
+    def __execute_task(self):
+        for queue_entry in self._queue_entry_list:
+            try:
+                self.__execute_entry(queue_entry)
+            except Exception as ex:
+                try:
+                    self.stop()
+                except gevent.GreenletExit:
+                    pass
+
+                logging.getLogger('user_level_log').error('Error executing ' +\
+                                                          'queue' + ex.message)
+                raise ex
+        
+        self.emit('queue_execution_finished', (None,))
+ 
 
     def __execute_entry(self, entry):
         self.set_current_entry(entry)
@@ -46,9 +121,6 @@ class QueueController(HardwareObject, QueueEntryContainer):
         entry.pre_execute()
 
         try:
-            #self.__queue_controller.current_executing_tasks.\
-            #    append(gevent.spawn(self.execute))
-            #self.__queue_controller.current_executing_tasks[-1].join()
             entry.execute()
         except:
             # This is definetly not good state, but call post_execute
@@ -71,26 +143,15 @@ class QueueController(HardwareObject, QueueEntryContainer):
                 raise
             
         entry.post_execute()
-
-            
-    def __execute_task(self):
-        for queue_entry in self._queue_entry_list:
-            try:
-                self.__execute_entry(queue_entry)
-            except Exception as ex:
-                try:
-                    self.stop()
-                except gevent.GreenletExit:
-                    pass
-
-                logging.getLogger('user_level_log').error('Error executing ' +\
-                                                          'queue' + ex.message)
-                raise ex
-        
-        self.emit('queue_execution_finished', (None,))
         
 
     def stop(self):
+        """
+        Stops the queue execution.
+
+        :returns: None
+        :rtype: NoneType
+        """
         self.get_current_entry().stop()
         # Reset the pause event, incase we were waiting.
         self.set_pause(False)
@@ -99,6 +160,16 @@ class QueueController(HardwareObject, QueueEntryContainer):
 
 
     def set_pause(self, state):
+        """
+        Sets the queue in paused state <state>. Emits the signal queue_paused
+        with the current state as parameter.
+
+        :param state: Paused if True running if False
+        :type state: bool
+
+        :returns: None
+        :rtype: NoneType
+        """
         self.emit('queue_paused', (state,))
         if state:
             self._paused_event.clear()
@@ -107,27 +178,71 @@ class QueueController(HardwareObject, QueueEntryContainer):
 
 
     def is_paused(self):
+        """
+        Returns the pause state, see the method set_pause().
+
+        :returns: None
+        :rtype: NoneType
+        """
         return not self._paused_event.is_set()
 
 
     def pause(self, state):
+        """
+        Sets the queue in paused state <state> (and waits), paused if True
+        running if False.
+
+        :param state: Paused if True running if False
+        :type state: bool
+        
+        :returns: None
+        :rtype: NoneType
+        """
         self.set_pause(state)
         self._paused_event.wait()
 
 
     def wait_for_pause_event(self):
+        """
+        Wait for the queue to be set to running set_pause(False) or continue if
+        it already was running.
+
+        :returns: None
+        :rtype: NoneType
+        """
         self._paused_event.wait()
 
 
     def set_current_entry(self, entry):
+        """
+        Sets the currently executing QueueEntry to <entry>.
+
+        :param entry: The entry.
+        :type entry: QeueuEntry
+
+        :returns: None
+        :rtype: NoneType
+        """
         self._current_queue_entry = entry
 
 
     def get_current_entry(self):
+        """
+        Gets the currently executing QueueEntry.
+
+        :returns: The currently executing QueueEntry:
+        :rtype: QueueEntry
+        """
         return self._current_queue_entry
 
 
     def clear(self):
+        """
+        Clears the queue (removes all entries).
+
+        :returns: None
+        :rtype: NoneType
+        """
         self._queue_entry_list = []
 
 
