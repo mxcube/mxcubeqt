84828e47fa85139f36485e465c99e55cf0fd2717
Marcus Oskarsson <oscarsso@linoscarsson.esrf.fr>
2013-06-07 13:27:47 +0200

    Commenting out the prefix member, since it should not be used anymore.

--- HardwareObjects/queue_model_objects_v1.py
+++ HardwareObjects/queue_model_objects_v1.py
@@ -654,7 +654,7 @@ class PathTemplate(object):
         self.directory = str()
         self.process_directory = str()
 
-        self.prefix = str()
+        #self.prefix = str()
         self.base_prefix = str()
         self.mad_prefix = str()
         self.reference_image_prefix = str()
