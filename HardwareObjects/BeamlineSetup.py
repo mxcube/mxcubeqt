c19b60f4f232cfca2c427f5a2bbe44dd67b2ce6d
bliss administrator <blissadm@regnum.esrf.fr>
2013-06-25 11:27:39 +0200

    Added hardware objects and a method for reading values.

--- HardwareObjects/BeamlineSetup.py
+++ HardwareObjects/BeamlineSetup.py
@@ -3,3 +3,32 @@ from HardwareRepository.BaseHardwareObjects import HardwareObject
 class BeamlineSetup(HardwareObject):
     def __init__(self, name):
         HardwareObject.__init__(self, name)
+        self._object_by_path = {}
+
+
+    def init(self):
+        self.diffractometer_hwobj = self.getObjectByRole('diffractometer')
+        self.shape_history_hwobj = self.getObjectByRole('shape_history')
+        self.energy_hwobj = self.getObjectByRole('energy_scan')
+        self.transmission_hwobj = self.getObjectByRole('transmission')
+        self.resolution_hwobj = self.getObjectByRole('resolution')                
+        self.session_hwobj = self.getObjectByRole('session')        
+        self.workflow_hwobj = self.getObjectByRole('workflow')
+
+
+        self._object_by_path['/beamline/energy'] = self.energy_hwobj
+        self._object_by_path['/beamline/resolution'] = self.resolution_hwobj
+        self._object_by_path['/beamline/transmission'] = self.transmission_hwobj
+
+
+    def read_value(self, path):
+        hwobj = None
+
+        try:
+            hwobj = self._object_by_path[path]
+        except KeyError:
+            raise ArgumentException('Invalid path')
+
+        return hwobj.get_value()
+            
+        
