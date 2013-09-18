d86e3ae58456e9f35c02803aee4556b9805129dd
bliss administrator <blissadm@p1-id29.esrf.fr>
2013-09-18 18:16:24 +0200

    Added a == comparison operator overload for centered positions.

--- HardwareObjects/queue_model_objects_v1.py
+++ HardwareObjects/queue_model_objects_v1.py
@@ -835,6 +835,13 @@ class CentredPosition(object):
                     'phiy': str(self.phiy),
                     'zoom': str(self.zoom)})
 
+    def __eq__(self, cpos):
+        result = (self.sampx == cpos.sampx) and (self.sampy == cpos.sampy) and \
+                 (self.phi == cpos.phi) and (self.phiz == cpos.phiz) and \
+                 (self.phiy == cpos.phiy) and (self.zoom == cpos.zoom)
+
+        return result
+
 
 class Workflow(TaskNode):
     def __init__(self):
