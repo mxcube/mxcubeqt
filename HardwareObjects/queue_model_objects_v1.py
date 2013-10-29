88f58c35c4d2b17a8211d1049826a100910fdd6b
Marcus Oskarsson <oscarsso@linoscarsson.esrf.fr>
2013-10-29 10:50:02 +0100

    Renamed is_part_of to intersection, New implementation of intersection (is_part_of) which is much faster.

--- HardwareObjects/queue_model_objects_v1.py
+++ HardwareObjects/queue_model_objects_v1.py
@@ -751,14 +751,16 @@ class PathTemplate(object):
 
         return result
 
-    def is_part_of(self, rh_pt):
+    def intersection(self, rh_pt):
         result = False
 
-        lh_set = set(self.get_files_to_be_written())
-        rh_set = set(rh_pt.get_files_to_be_written())
+        #Only do the intersection if there is possibilty for
+        #Collision, that is directories are the same.
+        if (self == rh_pt) and (self.run_number == rh_pt.run_number):
+            if self.start_num <= (rh_pt.start_num + rh_pt.num_files) and \
+               rh_pt.start_num <= (self.start_num + self.num_files):
 
-        if rh_set.intersection(lh_set):
-            result = True
+               result = True
     
         return result
 
