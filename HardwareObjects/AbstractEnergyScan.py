832f71a0917c077a0adb5897e522f3d75f300560
bliss administrator <blissadm@p1-id29new.esrf.fr>
2014-06-24 15:23:15 +0200

    added new energy scan files

---
+++ HardwareObjects/AbstractEnergyScan.py
@@ -0,0 +1,219 @@
+import os
+import sys
+import time
+import abc
+import logging
+from HardwareRepository.TaskUtils import *
+
+
+class AbstractEnergyScan(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        self.data_collect_task = None
+        self.scanning = False
+
+    @abc.abstractmethod
+    @task
+    def open_safety_shutter(self, timeout):
+        """
+        Open the safety shutter. Give a timeout [s] if needed.
+        """
+        pass
+
+
+    @abc.abstractmethod
+    @task
+    def close_safety_shutter(self, timeout):
+        """
+        Close the safety shutter. Give a timeout [s] if needed.
+        """
+        pass
+
+
+    @abc.abstractmethod
+    @task
+    def open_fast_shutter(self):
+        """
+        Open the fast shutter.
+        """
+        pass
+
+
+    @abc.abstractmethod
+    @task
+    def close_fast_shutter(self):
+        """
+        Close the fast shutter.
+        """
+
+        pass
+
+    @abc.abstractmethod
+    @task
+    def energy_scan_hook(self, energy_scan_parameters):
+        """
+        Execute actions, required before running the raw scan(like changing
+        undulator gaps, move to a given energy... These are in general
+        beamline specific actions.
+        """ 
+        pass
+
+
+    @abc.abstractmethod
+    @task
+    def execute_energy_scan(self, energy_scan_parameters):
+        """
+        Execute the raw scan sequence. Here is where you pass whatever
+        parameters you need to run the raw scan (e.g start/end energy,
+        counting time, energy step...).
+        """       
+        pass
+
+    @abc.abstractmethod
+    @task
+    def get_static_parameters(self, element, edge):
+        """
+        Get any parameters, which are known before hand. Some of them are
+        known from the theory, like the peak energy, others are equipment
+        specific like the lower/upper ROI limits of the fluorescence detector.
+        Usually these parameters are pre-defined in a file, but can also be
+        calculated.
+        The function should return a distionary with at least defined
+        {'edgeEnergy': peak_energy} member, where 'edgeEnergy' is a
+        compulsory key.
+        It is convinient to put in the same dictionary the remote energy,
+        the ROI min/max values.
+        There are few more reserved key names:
+        'eroi_min', 'eroi_max' - min and max ROI limits if you want ot set one.
+        'attEnergy' - energy to move to if you want to choose the attenuation
+        for the scan.
+        """
+        pass
+
+    @abc.abstractmethod
+    @task
+    def set_mca_roi(self, eroi_min, eroi_max):
+        """
+        Configure the fluorescent detector ROI. The input is min/max energy.
+        """
+        pass
+
+    @abc.abstractmethod
+    @task
+    def calculate_und_gaps(self, energy, undulator=None):
+        """
+        Calculate the undulator(s) gap(s), If specified, undulator is the
+        name of the undulator to chose if several possibilities. Return
+        a dictionary undulator:gap in the order undulator(s)should move.
+        """
+        pass
+
+    @abc.abstractmethod
+    @task
+    def move_undulators(self, undulators):
+        """
+        Move the undulator(s) to gap(s), where undulators is a dictionary
+        undulator:gap and should be in the order of which motor to move first.
+        """
+        pass
+
+    @abc.abstractmethod
+    @task
+    def escan_prepare(self):
+        """
+        Set the nesessary equipment in position for the scan. No need to know the c=scan paramets.
+        """
+        pass
+
+    @abc.abstractmethod
+    @task
+    def choose_attenuation(self, energy_scan_parameters):
+        """
+        Procedure to set the minimal attenuation in order no preserve
+        the sample. Should be done at the energy after the edge.
+        """
+        pass
+
+    @abc.abstractmethod
+    @task
+    def move_energy(self, energy):
+        """
+        Move the monochromator to energy - used before and after the scan.
+        """
+        pass
+
+    @abc.abstractmethod
+    @task
+    def escan_cleanup(self):
+        pass
+
+    @abc.abstractmethod
+    @task
+    def escan_postscan(self):
+        """
+        set the nesessary equipment in position after the scan
+        """
+        pass
+              
+    @task
+    #def do_energy_scan(self, energy_scan_parameters):
+    def startEnergyScan(self,element,edge,directory,prefix,session_id=None,blsample_id=None):
+
+        STATICPARS_DICT = {}
+        #Set the energy from the element and edge parameters
+        STATICPARS_DICT = self.get_static_parameters(element,edge)
+        
+        #Calculate the MCA ROI (if needed)
+        try:
+            self.set_mca_roi(STATICPARS_DICT['eroi_min'], STATICPARS_DICT['eroi_max'])
+        except:
+            pass
+        #Calculate undulator gaps (if any)
+        GAPS = {}
+        try:
+            GAPS = self.calculate_und_gaps(STATICPARS_DICT['edgeEnergy'], und=None)
+        except:
+            pass
+
+        self.energy_scan_parameters = STATICPARS_DICT
+        self.energy_scan_parameters["element"] = element
+        self.energy_scan_parameters["edge"] = edge
+        self.energy_scan_parameters["directory"] = directory
+        self.energy_scan_parameters["prefix"]=prefix
+        if session_id is not None:
+            self.energy_scan_parameters["sessionId"] = session_id
+            self.energy_scan_parameters["blSampleId"] = blsample_id
+            self.energy_scan_parameters['startTime']=time.strftime("%Y-%m-%d %H:%M:%S")
+
+        with cleanup(self.escan_cleanup):
+
+            self.escan_prepare()
+            try:
+                self.move_energy(self.energy_scan_parameters['attEnergy'])
+            except:
+                pass
+            self.energy_scan_hook(self.energy_scan_parameters)
+            self.open_safety_shutter(timeout=10)
+            self.choose_attenuation()
+            self.close_fast_shutter()
+            logging.getLogger("HWR").debug("Doing the scan, please wait...")
+            self.execute_energy_scan(self.energy_scan_parameters)
+            self.escan_postscan()
+            
+        self.close_fast_shutter()
+        self.close_safety_shutter(timeout=10)
+        #send finish sucessfully signal to the brick
+        self.emit('energyScanFinished', (self.energy_scan_parameters,))
+        self.ready_event.set()
+        
+    @task
+    def doChooch(self, elememt, edge, scanArchiveFilePrefix, scanFilePrefix):
+        """
+        Use chooch to calculate edge and inflection point
+        The brick expects the folowing parameters to be returned:
+        pk, fppPeak, fpPeak, ip, fppInfl, fpInfl, rm,
+        chooch_graph_x, chooch_graph_y1, chooch_graph_y2, title)
+        """
+        pass
+
---
+++ HardwareObjects/ESRFEnergyScan.py
@@ -0,0 +1,348 @@
+from HardwareRepository.BaseHardwareObjects import HardwareObject
+from AbstractEnergyScan import *
+from gevent.event import AsyncResult
+import logging
+import time
+import os
+import httplib
+import math
+import calc_gaps
+import PyChooch
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+
+class FixedEnergy:
+
+    @task
+    def get_energy(self):
+        return self._tunable_bl.energy_obj.getPosition()
+
+
+class TunableEnergy:
+
+    @task
+    def get_energy(self):
+        return self._tunable_bl.energy_obj.getCurrentEnergy()
+
+    @task
+    def move_energy(self, energy):
+        return self._tunable_bl.energy_obj.startMoveEnergy(energy, wait=True)
+
+    
+class GetStaticParameters:
+    def __init__(self, element, edge):
+        self.element = element
+        self.edge = edge
+        config_file = "/users/blissadm/local/spec/userconf/EdgeScan.dat"
+        self.STATICPARS_DICT = {}
+        self.STATICPARS_DICT = self._readParamsFromFile(config_file)
+
+    def _readParamsFromFile(self, config_file):
+        try:
+            f = open(config_file)
+            array = []
+            for line in f:
+                if not line.startswith('#'):
+                    array.append(line.split())
+                else:
+                    pass
+        except:
+            return []
+        else:
+            larr = []
+            for k in range(len(array)):
+                if self.element == array[k][1] and self.edge[0] == array[k][2]:
+                    larr = map(float,array[k][3:13])
+                    larr.append(float(array[k][17]))
+            if self.edge == "K":
+                to_delete = [1,2,3,4,5,6,7]
+            else:
+                try:
+                    if int(self.edge[1]) == 1:
+                        to_delete = [0,1,2,4,5,6,7]
+                    elif int(self.edge[1]) == 2:
+                        to_delete = [0,1,2,3,5,6,7]
+                    else:
+                        to_delete = [0,1,2,3,4,6,7]
+                except:
+                    to_delete = [0,1,2,3,4,6,7]
+            for ii in sorted(to_delete, reverse=True):
+                del larr[ii]
+
+            static_pars = {}
+            larr[0] /= 1000
+            static_pars["edgeEnergy"] = larr[0]
+            static_pars["startEnergy"] = larr[0] - 0.05
+            static_pars["endEnergy"] = larr[0] + 0.05
+            static_pars["findattEnergy"] = larr[0] + 0.03
+            static_pars["remoteEnergy"] = larr[0] + 1
+            static_pars["eroi_min"] = larr[1]
+            static_pars["eroi_max"] = larr[2]
+            
+            return static_pars
+        
+class ESRFEnergyScan(AbstractEnergyScan, HardwareObject):
+    def __init__(self, name, tunable_bl):
+        AbstractEnergyScan.__init__(self)
+        HardwareObject.__init__(self, name)
+        self._tunable_bl = tunable_bl
+        
+    def execute_command(self, command_name, *args, **kwargs): 
+        wait = kwargs.get("wait", True)
+        cmd_obj = self.getCommandObject(command_name)
+        return cmd_obj(*args, wait=wait)
+
+    def init(self):
+        self.energy_obj =  self.getObjectByRole("energy")
+        self.safety_shutter = self.getObjectByRole("safety_shutter")
+        self.beamsize = self.getObjectByRole("beamsize")
+        self.transmission = self.getObjectByRole("transmission")
+        self.ready_event = gevent.event.Event()
+        self.dbConnection=self.getObjectByRole("dbserver")
+        if self.dbConnection is None:
+            logging.getLogger("HWR").warning('EnergyScan: you should specify the database hardware object')
+        self.scanInfo=None
+        self._tunable_bl.energy_obj = self.energy_obj
+
+    def isConnected(self):
+        return True
+
+    @task
+    def get_static_parameters(self, element, edge):
+
+        pars = GetStaticParameters(element, edge).STATICPARS_DICT
+        
+        offset_keV = self.getProperty("offset_keV")
+        pars["startEnergy"] += offset_keV
+        pars["endEnergy"] += offset_keV
+
+        #next to tell spec which energy
+        self.getChannelObject("ae_rcm").setValue(pars)
+        self.execute_command("setRcmValues")
+        
+        return pars
+
+    @task
+    def open_safety_shutter(self):
+        self.safety_shutter.openShutter()
+        while self.safety_shutter.getShutterState() == 'closed':
+            time.sleep(0.1)
+
+
+    @task
+    def close_safety_shutter(self):
+        self.safety_shutter.closeShutter()
+        while self.safety_shutter.getShutterState() == 'opened':
+            time.sleep(0.1)
+
+    @task
+    def escan_prepare(self):
+        self.execute_command("presetScan")
+        bsX = self.beamsize.getCurrentPositionName()
+        bsY = bsX
+        self.energy_scan_parameters["beamSizeHorizontal"] = bsX
+        self.energy_scan_parameters["beamSizeVertical"]=bsY
+
+    @task
+    def escan_postscan(self):
+        self.execute_command("cleanScan")
+        
+    @task
+    def escan_cleanup(self):
+        self.close_fast_shutter()
+        self.close_safety_shutter()
+        self.execute_command("cleanScan")
+        self.ready_event.set()
+
+    @task
+    def close_fast_shutter(self):
+        self.execute_command("close_fast_shutter")
+
+    @task
+    def open_fast_shutter(self):
+        self.execute_command("open_fast_shutter")
+
+    def move_energy(self, energy):
+        return self.energy_obj.startMoveEnergy(energy, wait=True)
+        #return self._tunable_bl.energy_obj.move_energy(energy)
+
+    # Elements commands
+    def getElements(self):
+        elements=[]
+        try:
+            for el in self["elements"]:
+                elements.append({"symbol":el.symbol, "energy":el.energy})
+        except IndexError:
+            pass
+
+        return elements
+
+    def storeEnergyScan(self):
+        if self.dbConnection is None:
+            return
+        try:
+            session_id=int(self.energy_scan_parameters['sessionId'])
+        except:
+            return
+
+        #remore unnecessary for ISPyB fields:
+        self.energy_scan_parameters.pop('prefix')
+        self.energy_scan_parameters.pop('eroi_min')
+        self.energy_scan_parameters.pop('eroi_max')
+        self.energy_scan_parameters.pop('findattEnergy')
+
+        gevent.spawn(StoreEnergyScanThread, self.dbConnection,self.energy_scan_parameters)
+
+    def doChooch(self, elt, edge, scanArchiveFilePrefix, scanFilePrefix):
+        symbol = "_".join((elt, edge))
+        scanArchiveFilePrefix = "_".join((scanArchiveFilePrefix, symbol))
+
+        i = 1
+        while os.path.isfile(os.path.extsep.join((scanArchiveFilePrefix + str(i), "raw"))):
+            i = i + 1
+
+        scanArchiveFilePrefix = scanArchiveFilePrefix + str(i) 
+        archiveRawScanFile=os.path.extsep.join((scanArchiveFilePrefix, "raw"))
+        rawScanFile=os.path.extsep.join((scanFilePrefix, "raw"))
+        scanFile=os.path.extsep.join((scanFilePrefix, "efs"))
+
+        if not os.path.exists(os.path.dirname(scanArchiveFilePrefix)):
+            os.makedirs(os.path.dirname(scanArchiveFilePrefix))
+        
+        try:
+            f=open(rawScanFile, "w")
+            pyarch_f=open(archiveRawScanFile, "w")
+        except:
+            logging.getLogger("HWR").exception("could not create raw scan files")
+            self.storeEnergyScan()
+            self.emit("energyScanFailed", ())
+            return
+        else:
+            scanData = []
+            
+            raw_data_file = os.path.join(os.path.dirname(scanFilePrefix), 'data.raw')
+            try:
+                raw_file = open(raw_data_file, 'r')
+            except:
+                self.storeEnergyScan()
+                self.emit("energyScanFailed", ())
+                return
+
+            for line in raw_file.readlines()[2:]:
+                (x, y) = line.split('\t')
+                x = float(x.strip())
+                y = float(y.strip())
+                #x = x < 1000 and x*1000.0 or x
+                scanData.append((x, y))
+                f.write("%f,%f\r\n" % (x, y))
+                pyarch_f.write("%f,%f\r\n"% (x, y))
+
+
+            f.close()
+            pyarch_f.close()
+            self.energy_scan_parameters["scanFileFullPath"]=str(archiveRawScanFile)
+
+        pk, fppPeak, fpPeak, ip, fppInfl, fpInfl, chooch_graph_data = PyChooch.calc(scanData, elt, edge, scanFile)
+        rm=(pk+30)/1000.0
+        pk=pk/1000.0
+        savpk = pk
+        ip=ip/1000.0
+        comm = ""
+        self.thEdge = self.energy_scan_parameters['edgeEnergy']
+        logging.getLogger("HWR").info("th. Edge %s ; chooch results are pk=%f, ip=%f, rm=%f" % (self.thEdge, pk,ip,rm))
+
+        #should be better, but OK for time being
+        self.thEdgeThreshold = 0.01
+        if math.fabs(self.thEdge - ip) > self.thEdgeThreshold:
+          pk = 0
+          ip = 0
+          rm = self.thEdge + 0.03
+          comm = 'Calculated peak (%f) is more that 10eV away from the theoretical value (%f). Please check your scan' % (savpk, self.thEdge)
+   
+          logging.getLogger("HWR").warning('EnergyScan: calculated peak (%f) is more that 20eV %s the theoretical value (%f). Please check your scan and choose the energies manually' % (savpk, (self.thEdge - ip) > 0.02 and "below" or "above", self.thEdge))
+
+        archiveEfsFile=os.path.extsep.join((scanArchiveFilePrefix, "efs"))
+        try:
+          fi=open(scanFile)
+          fo=open(archiveEfsFile, "w")
+        except:
+          self.storeEnergyScan()
+          self.emit("energyScanFailed", ())
+          return
+        else:
+          fo.write(fi.read())
+          fi.close()
+          fo.close()
+
+        self.energy_scan_parameters["peakEnergy"]=pk
+        self.energy_scan_parameters["inflectionEnergy"]=ip
+        self.energy_scan_parameters["remoteEnergy"]=rm
+        self.energy_scan_parameters["peakFPrime"]=fpPeak
+        self.energy_scan_parameters["peakFDoublePrime"]=fppPeak
+        self.energy_scan_parameters["inflectionFPrime"]=fpInfl
+        self.energy_scan_parameters["inflectionFDoublePrime"]=fppInfl
+        self.energy_scan_parameters["comments"] = comm
+
+        chooch_graph_x, chooch_graph_y1, chooch_graph_y2 = zip(*chooch_graph_data)
+        chooch_graph_x = list(chooch_graph_x)
+        for i in range(len(chooch_graph_x)):
+          chooch_graph_x[i]=chooch_graph_x[i]/1000.0
+
+        logging.getLogger("HWR").info("<chooch> Saving png" )
+        # prepare to save png files
+        title="%10s  %6s  %6s\n%10s  %6.2f  %6.2f\n%10s  %6.2f  %6.2f" % ("energy", "f'", "f''", pk, fpPeak, fppPeak, ip, fpInfl, fppInfl) 
+        fig=Figure(figsize=(15, 11))
+        ax=fig.add_subplot(211)
+        ax.set_title("%s\n%s" % (scanFile, title))
+        ax.grid(True)
+        ax.plot(*(zip(*scanData)), **{"color":'black'})
+        ax.set_xlabel("Energy")
+        ax.set_ylabel("MCA counts")
+        ax2=fig.add_subplot(212)
+        ax2.grid(True)
+        ax2.set_xlabel("Energy")
+        ax2.set_ylabel("")
+        handles = []
+        handles.append(ax2.plot(chooch_graph_x, chooch_graph_y1, color='blue'))
+        handles.append(ax2.plot(chooch_graph_x, chooch_graph_y2, color='red'))
+        canvas=FigureCanvasAgg(fig)
+
+        escan_png = os.path.extsep.join((scanFilePrefix, "png"))
+        escan_archivepng = os.path.extsep.join((scanArchiveFilePrefix, "png")) 
+        self.energy_scan_parameters["jpegChoochFileFullPath"]=str(escan_archivepng)
+        try:
+          logging.getLogger("HWR").info("Rendering energy scan and Chooch graphs to PNG file : %s", escan_png)
+          canvas.print_figure(escan_png, dpi=80)
+        except:
+          logging.getLogger("HWR").exception("could not print figure")
+        try:
+          logging.getLogger("HWR").info("Saving energy scan to archive directory for ISPyB : %s", escan_archivepng)
+          canvas.print_figure(escan_archivepng, dpi=80)
+        except:
+          logging.getLogger("HWR").exception("could not save figure")
+
+        self.energy_scan_parameters['endTime']=time.strftime("%Y-%m-%d %H:%M:%S")
+        self.storeEnergyScan()
+
+        logging.getLogger("HWR").info("<chooch> returning" )
+        #self.emit('chooch_finished', (pk, fppPeak, fpPeak, ip, fppInfl, fpInfl, rm, chooch_graph_x, chooch_graph_y1, chooch_graph_y2, title))
+        return pk, fppPeak, fpPeak, ip, fppInfl, fpInfl, rm, chooch_graph_x, chooch_graph_y1, chooch_graph_y2, title
+
+def StoreEnergyScanThread(db_conn, scan_info):
+    scanInfo = dict(scan_info)
+    dbConnection = db_conn
+
+    import pdb; pdb.set_trace()
+    blsampleid = scanInfo['blSampleId']
+    scanInfo.pop('blSampleId')
+    db_status=dbConnection.storeEnergyScan(scanInfo)
+    if blsampleid is not None:
+        try:
+            energyscanid=int(db_status['energyScanId'])
+        except:
+            pass
+        else:
+            asoc={'blSampleId':blsampleid, 'energyScanId':energyscanid}
+            dbConnection.associateBLSampleAndEnergyScan(asoc)
+ 
