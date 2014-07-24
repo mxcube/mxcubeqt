e1b460b279c5919664ae14178b1a1b443b7e7a7b
bliss administrator <blissadm@capek.esrf.fr>
2014-07-24 12:00:11 +0200

    added diffractometer motor names in Robodiff init added helical_oscil added roby,robz motors

--- HardwareObjects/Robodiff.py
+++ HardwareObjects/Robodiff.py
@@ -5,22 +5,44 @@ import os
 import sys
 import logging
 import time
+import queue_model_objects_v1 as qmo
 
 class Robodiff(MiniDiff.MiniDiff):      
     def __init__(self, name):
         MiniDiff.MiniDiff.__init__(self, name)
+        qmo.CentredPosition.set_diffractometer_motor_names("phi",
+                                                           "focus",
+                                                           "phiz",
+                                                           "phiy",
+                                                           "zoom",
+                                                           "sampx",
+                                                           "sampy",
+                                                           "kappa",
+                                                           "kappa_phi",
+                                                           "chi",
+                                                           "roby",
+                                                           "robz")
+
+
 
     def init(self): 
         self.controller = self.getObjectByRole("controller")
         MiniDiff.MiniDiff.init(self)
         self.chiMotor = self.getDeviceByRole('chi')
+        self.robzMotor = self.getDeviceByRole('robz')
+        self.robyMotor = self.getDeviceByRole('roby')
 
     def oscil(self, *args, **kwargs):
         self.controller.oscil(*args, **kwargs)
 
+    def helical_oscil(self, *args, **kwargs):
+        self.controller.helical_oscil(*args, **kwargs)
+
     def getPositions(self, *args, **kwargs):
         res=MiniDiff.MiniDiff.getPositions(self,*args, **kwargs)
         res["chi"]=self.chiMotor.getPosition()
+        res["roby"]=self.robyMotor.getPosition()
+        res["robz"]=self.robzMotor.getPosition()
         return res
 
     def start3ClickCentring(self, sample_info=None):
@@ -64,7 +86,10 @@ class Robodiff(MiniDiff.MiniDiff):
                   "kappa": self.kappaMotor,
                   "kappa_phi": self.kappaPhiMotor,
                   "chi": self.chiMotor,
+                  "roby": self.robyMotor,
+                  "robz": self.robzMotor,
                   "zoom": self.zoomMotor }
+
 
         for role, pos in roles_positions_dict.iteritems():
            motor[role].move(pos)
