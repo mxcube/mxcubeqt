b66c27b94b30b493441f181a3998f927404d19ba
bliss administrator <blissadm@aelita.esrf.fr>
2014-12-10 15:20:19 +0100

    Fix for missing space character problem

--- HardwareObjects/autoprocessing.py
+++ HardwareObjects/autoprocessing.py
@@ -66,7 +66,7 @@ def start(programs, processEvent, paramsDict):
 		    #if opts is not None:
 			#lineToExecute = executable + ' ' + opts + endOfLineToExecute
 		    #else:
-                    lineToExecute = executable + endOfLineToExecute + '2>&1 > /dev/null &'
+                    lineToExecute = executable + endOfLineToExecute + ' 2>&1 > /dev/null &'
 		    logging.info("Process event %s, executing %s" % (processEvent,str(lineToExecute)))
 
 		    # os.system is preferred to subprocess because we want to detach
