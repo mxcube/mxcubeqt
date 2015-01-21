cf5de6c60d6ffffff41d9b71f854389cbf8acbb7
Ivars Karpics <ivars.karpics@gmail.com>
2015-01-21 13:49:48 +0100

    2015-01-21 - Kappa and phi control Brick
            * Bricks/KappaPhiMotorsBrick.py: add value input validator.

--- Bricks/KappaPhiMotorsBrick.py
+++ Bricks/KappaPhiMotorsBrick.py
@@ -1,12 +1,16 @@
-from BlissFramework import BaseComponents
-from BlissFramework import Icons
-from qt import *
+import qt
 import logging
+
+from BlissFramework import Icons
+from BlissFramework.BaseComponents import BlissWidget
+
 from BlissFramework.Utils import widget_colors
+
 
 __category__ = 'Motor'
 
-class KappaPhiMotorsBrick(BaseComponents.BlissWidget):
+
+class KappaPhiMotorsBrick(BlissWidget):
 
     STATE_COLORS = (widget_colors.LIGHT_RED, 
                     widget_colors.DARK_GRAY,
@@ -18,12 +22,9 @@ class KappaPhiMotorsBrick(BaseComponents.BlissWidget):
     MAX_HISTORY = 20
 
     def __init__(self,*args):
-        BaseComponents.BlissWidget.__init__(self,*args)
+        BlissWidget.__init__(self,*args)
 
-        self.stepEditor=None
         self.diffractometer_hwobj=None
-        self.demandMove=0
-        self.inExpert=None
 
         self.addProperty('mnemonic','string','')
         self.addProperty('label','string','')
@@ -33,40 +34,49 @@ class KappaPhiMotorsBrick(BaseComponents.BlissWidget):
         self.addProperty('showPosition', 'boolean', True)
         self.addProperty('icons', 'string', '')
 
-        self.containerBox=QHGroupBox(self)
-        self.containerBox.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        self.container_hbox = qt.QHGroupBox(self)
+        self.container_hbox.setSizePolicy(qt.QSizePolicy.Minimum, qt.QSizePolicy.Fixed)
 
-        self.labelBox=QHBox(self.containerBox)
-        self.motorBox=QHBox(self.containerBox)
+        self.label_hbox = qt.QHBox(self.container_hbox)
+        self.motor_hbox = qt.QHBox(self.container_hbox)
 
-        self.label=QLabel(self.labelBox)
+        self.label = qt.QLabel(self.label_hbox)
 
-        self.kappaValueEdit = QLineEdit(self.motorBox)
-        self.kappaValueEdit.setFixedSize(QSize(55,25))
-        self.phiValueEdit = QLineEdit(self.motorBox)
-        self.phiValueEdit.setFixedSize(QSize(55,25))
-        self.moveToPositionsButton = QPushButton("Apply", self.motorBox)  
-        self.stopButton = QPushButton(self.motorBox)
-        self.stopButton.setPixmap(Icons.load('stop_small'))
-        self.stopButton.setEnabled(False)
+        self.kappa_value_edit = qt.QLineEdit(self.motor_hbox)
+        self.kappa_value_edit.setFixedSize(qt.QSize(55,25))
+        self.kappa_value_edit.setValidator(qt.QDoubleValidator(0, 180, 2, self))
+        self.kappa_value_edit.setPaletteBackgroundColor(widget_colors.LIGHT_GREEN)
+        self.kappa_value_edit.setAlignment(qt.QWidget.AlignRight)
+        self.phi_value_edit = qt.QLineEdit(self.motor_hbox)
+        self.phi_value_edit.setFixedSize(qt.QSize(55,25))
+        self.phi_value_edit.setValidator(qt.QDoubleValidator(0, 180, 2, self))
+        self.phi_value_edit.setPaletteBackgroundColor(widget_colors.LIGHT_GREEN)
+        self.phi_value_edit.setAlignment(qt.QWidget.AlignRight)
 
-        self.connect(self.moveToPositionsButton, SIGNAL('clicked()'), self.moveToPositions)
-        self.connect(self.stopButton, SIGNAL('clicked()'), self.stopMotors)  
+        self.move_motors_button = qt.QPushButton("Apply", self.motor_hbox)  
+        self.stop_motors_button = qt.QPushButton(self.motor_hbox)
+        self.stop_motors_button.setPixmap(Icons.load('stop_small'))
+        self.stop_motors_button.setEnabled(False)
+
+        self.connect(self.kappa_value_edit, qt.SIGNAL('textChanged(const QString &)'), self.kappa_edit_input_changed)
+        self.connect(self.phi_value_edit, qt.SIGNAL('textChanged(const QString &)'), self.phi_edit_input_changed)
+        self.connect(self.move_motors_button, qt.SIGNAL('clicked()'), self.move_motors_button_clicked)
+        self.connect(self.stop_motors_button, qt.SIGNAL('clicked()'), self.stop_motors_button_clicked)  
        
-        self.stopButton.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Minimum)
-        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        QVBoxLayout(self)
-        self.layout().addWidget(self.containerBox)
+        self.stop_motors_button.setSizePolicy(qt.QSizePolicy.Fixed, qt.QSizePolicy.Minimum)
+        self.setSizePolicy(qt.QSizePolicy.Minimum, qt.QSizePolicy.Fixed)
+        qt.QVBoxLayout(self)
+        self.layout().addWidget(self.container_hbox)
         
     def propertyChanged(self,propertyName,oldValue,newValue):
         if propertyName=='mnemonic':
             if self.diffractometer_hwobj is not None:
-                self.disconnect(self.diffractometer_hwobj, PYSIGNAL("kappaMoved"), self.kappaMoved) 
-                self.disconnect(self.diffractometer_hwobj, PYSIGNAL("phiMoved"), self.phiMoved)
+                self.disconnect(self.diffractometer_hwobj, qt.PYSIGNAL("kappa_value_changed"), self.kappa_value_changed) 
+                self.disconnect(self.diffractometer_hwobj, qt.PYSIGNAL("phiMoved"), self.phi_value_changed)
             self.diffractometer_hwobj = self.getHardwareObject(newValue)
             if self.diffractometer_hwobj is not None:
-                self.connect(self.diffractometer_hwobj, PYSIGNAL("kappaMoved"), self.kappaMoved)            
-                self.connect(self.diffractometer_hwobj, PYSIGNAL("phiMoved"), self.phiMoved)
+                self.connect(self.diffractometer_hwobj, qt.PYSIGNAL("kappa_value_changed"), self.kappa_value_changed)            
+                self.connect(self.diffractometer_hwobj, qt.PYSIGNAL("phiMoved"), self.phi_value_changed)
         elif propertyName=='label':
             self.setLabel(newValue)
         elif propertyName=='showLabel':
@@ -76,61 +86,77 @@ class KappaPhiMotorsBrick(BaseComponents.BlissWidget):
                 self.setLabel(None)
         elif propertyName=='showStop':
             if newValue:
-                self.stopButton.show()
+                self.stop_motors_button.show()
             else:
-                self.stopButton.hide()
+                self.stop_motors_button.hide()
         elif propertyName=='showBox':
             if newValue:
-                self.containerBox.setFrameShape(self.containerBox.GroupBoxPanel)
-                self.containerBox.setInsideMargin(4)
-                self.containerBox.setInsideSpacing(0)
+                self.container_hbox.setFrameShape(self.container_hbox.GroupBoxPanel)
+                self.container_hbox.setInsideMargin(4)
+                self.container_hbox.setInsideSpacing(0)
             else:
-                self.containerBox.setFrameShape(self.containerBox.NoFrame)
-                self.containerBox.setInsideMargin(0)
-                self.containerBox.setInsideSpacing(0)            
+                self.container_hbox.setFrameShape(self.container_hbox.NoFrame)
+                self.container_hbox.setInsideMargin(0)
+                self.container_hbox.setInsideSpacing(0)            
             self.setLabel(self['label'])
         elif propertyName=='icons':
             icons_list=newValue.split()
             try:
-                self.moveToPositionsButton.setPixmap(Icons.load(icons_list[0]))
+                self.move_motors_button.setPixmap(Icons.load(icons_list[0]))
             except IndexError:
                 pass                
         else:
-            BaseComponents.BlissWidget.propertyChanged(self,propertyName,oldValue,newValue)
+            BlissWidget.propertyChanged(self,propertyName,oldValue,newValue)
 
-    def stopMotors(self):
+    def stop_motors_button_clicked(self):
         self.diffractometer_hwobj.stop_kappa_phi()
 
-    def moveToPositions(self):
-        try:
-           kappa_value = float(self.kappaValueEdit.text())
-           phi_value = float(self.phiValueEdit.text())
-           self.diffractometer_hwobj.move_kapp_phi(kappa_value, phi_value)
-        except:
-           pass
+    def move_motors_button_clicked(self):
+        kappa_value = float(self.kappa_value_edit.text())
+        phi_value = float(self.phi_value_edit.text())
+        self.diffractometer_hwobj.move_kappa_phi(kappa_value, phi_value)
 
-    def kappaMoved(self, value):
-        self.kappaValueEdit.setText(str(value))
+        self.kappa_value_edit.setPaletteBackgroundColor(widget_colors.LIGHT_GREEN)
+        self.kappa_value_edit.clearFocus()
+        self.phi_value_edit.setPaletteBackgroundColor(widget_colors.LIGHT_GREEN)
+        self.phi_value_edit.clearFocus()
+
+    def kappa_edit_input_changed(self, new_text):
+        self.kappa_value_edit.setPaletteBackgroundColor(qt.QColor(255,165,0))
+
+    def phi_edit_input_changed(self, new_text):
+        self.phi_value_edit.setPaletteBackgroundColor(qt.QColor(255,165,0))
+
+    def kappa_value_changed(self, value):
+        self.kappa_value_edit.blockSignals(True)
+        txt = '?' if value is None else '%s' %\
+              str(self['formatString'] % value)
+        self.kappa_value_edit.setText(txt)
+        self.kappa_value_edit.blockSignals(False)
     
-    def phiMoved(self, value):
-        self.phiValueEdit.setText(str(value))   
+    def phi_value_changed(self, value):
+        self.phi_value_edit.blockSignals(True)
+        txt = '?' if value is None else '%s' %\
+              str(self['formatString'] % value)
+        self.phi_value_edit.setText(txt)
+        self.phi_value_edit.blockSignals(False)
 
     def setLabel(self,label):
         if not self['showLabel']:
             label=None
 
         if label is None:
-            self.labelBox.hide()
-            self.containerBox.setTitle("")
+            self.label_hbox.hide()
+            self.container_hbox.setTitle("")
             return
 
         if self['showBox']:
-            self.labelBox.hide()
-            self.containerBox.setTitle(label)
+            self.label_hbox.hide()
+            self.container_hbox.setTitle(label)
         else:
             if label!="":
                 label+=": "
-            self.containerBox.setTitle("")
+            self.container_hbox.setTitle("")
             self.label.setText(label)
-            self.labelBox.show()
+            self.label_hbox.show()
 
