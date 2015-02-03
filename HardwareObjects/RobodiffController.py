55edd40f4998cfb53ba862fa9c7ab0a1a9a9338a
bliss administrator <blissadm@capek.esrf.fr>
2015-02-03 13:27:47 +0100

    added ID30A1PhotonFlux removed get_flux and get_cryo_temperature methods overloading in ID30MultiCollect HWR submodule update RobodiffController's init() is cleaner fixed ObjectsController call to get_context_objects

--- HardwareObjects/RobodiffController.py
+++ HardwareObjects/RobodiffController.py
@@ -7,13 +7,7 @@ class RobodiffController(HardwareObject):
     HardwareObject.__init__(self, *args)
 
   def init(self, *args):  
-     sys.path.insert(0, self.getProperty("source"))
-     config = __import__("config", globals(), locals(), [])
-       
-     cfg_file = os.path.join(self.getProperty("source"), self.getProperty("config_file")) 
-     config.load(cfg_file)
-     objects = config.get_context_objects("default", "default")
-     self.__controller = objects["robot"]
+     self.__controller = self.getObjectByRole("controller").robot
 
   def set_diagfile(self, diagfile):
      self.__controller.diagfile = diagfile
