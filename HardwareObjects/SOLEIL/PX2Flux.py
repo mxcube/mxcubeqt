8f6a2b7b76092d3e7aa321640f68b86a56f3caa0
Martin Savko <savko@synchrotron-soleil.fr>
2015-04-20 12:20:04 +0200

    Making flux working on PX2A

--- HardwareObjects/SOLEIL/PX2Flux.py
+++ HardwareObjects/SOLEIL/PX2Flux.py
@@ -1,45 +1,70 @@
 
 from HardwareRepository import HardwareRepository
-from HardwareRepository.BaseHardwareObjects import Device
+from HardwareRepository import BaseHardwareObjects
 import logging
-import PyTango
 
-class PX2Flux(Device):
+class PX2Flux(BaseHardwareObjects.Device):
 
     def __init__(self, name):
-        Device.__init__(self, name)
+        BaseHardwareObjects.Device.__init__(self, name)
 
     def init(self):
-        logging.info('PX2Flux, initializing')
-        self.flux_channel = self.getChannelObject("flux")
-    
-    def getCurrentFlux(self):
-        
+
         try:
-            flx = self.flux_channel.getValue()/1e12
-            logging.info('PX2Flux, getCurrentFlux %s' % flx)
-            return flx
-            
+            self.fluxchan = self.getChannelObject('flux')
+            self.fluxchan.connectSignal('update', self.fluxChanged)
+            try:
+                self.flux = self.fluxchan.getValue()
+            except:
+                self.flux = -1
+            self.statuschan = self.getChannelObject('state')
+            self.statuschan.connectSignal('update', self.stateChanged)
+            logging.getLogger().debug('%s: connected to channels', self.name())
+        except KeyError:
+            logging.getLogger().warning('%s: cannot connect to channel', self.name())
+        except Exception, e:
+            print "FLUX INIT, ERROR MESSAGE: %s"  % e
+            logging.getLogger().info('%s: FLUX ERROR Message', self.name())            
+            import traceback 
+            logging.getLogger().info("flux is type: %s ", str(type(self.flux)))
+            #logging.info(traceback.format_exc())            #logging.getLogger().error('%s: Message: %s', e)
+
+    def getCurrentFlux(self):
+        try:
+            return self.fluxchan.getValue()
         except PyTango.DevFailed:
-            import traceback
-                
-            logging.info('PX2Flux, getCurrentFlux failed' % traceback.format_exc())
             return -1
+
+    def getValue(self):
+        return self.getCurrentFlux()
+
+    def fluxChanged(self, value):
+        #
+        # emit signal
+        #
+        self.flux = value
+        self.emit('fluxChanged', str("%7.1e" % value))
+        self.emit('valueChanged', str("%7.1e" % self.flux))
+
+    def stateChanged(self, value):
+        #
+        # emit signal
+        #
+        self.emit('fluxStatusChanged', value)
+        self.emit('stateChanged', value)
 
 
 def test():
     import os
-    import time
     hwr_directory = os.environ["XML_FILES_PATH"]
 
     hwr = HardwareRepository.HardwareRepository(os.path.abspath(hwr_directory))
     hwr.connect()
 
     flux = hwr.getHardwareObject("/flux")
-
     print "PX2 Flux is ",flux.getCurrentFlux()
 
 
 if __name__ == '__main__':
-   test()
+    test()
 
