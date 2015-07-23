231f9ed1dbe6e22fd2508667d647713205ccbf2f
beteva <beteva.esrf.fr>
2015-07-23 11:05:07 +0200

    Changes to cope with the MD2S library. Use front light only for the snapshots.

--- HardwareObjects/Microdiff.py
+++ HardwareObjects/Microdiff.py
@@ -23,11 +23,10 @@ class Microdiff(MiniDiff.MiniDiff):
         self.exporter_addr = self.phiMotor.exporter_address
         self.x_calib = self.addChannel({ "type":"exporter", "exporter_address": self.exporter_addr, "name":"x_calib" }, "CoaxCamScaleX")
         self.y_calib = self.addChannel({ "type":"exporter", "exporter_address": self.exporter_addr, "name":"y_calib" }, "CoaxCamScaleY")       
-        self.moveMultipleMotors = self.addCommand({"type":"exporter", "exporter_address":self.exporter_addr, "name":"move_multiple_motors" }, "SyncMoveMotors")
 
         self.phases = {"Centring":1, "BeamLocation":2, "DataCollection":3, "Transfer":4}
         self.movePhase = self.addCommand({"type":"exporter", "exporter_address":self.exporter_addr, "name":"move_to_phase" }, "startSetPhase")
-        self.readPhase =  self.addChannel({ "type":"exporter", "exporter_address": self.exporter_addr, "name":"read_phase" }, "CurrentPhaseIndex")
+        self.readPhase =  self.addChannel({ "type":"exporter", "exporter_address": self.exporter_addr, "name":"read_phase" }, "CurrentPhase")
         self.hwstate_attr = self.addChannel({"type":"exporter", "exporter_address": self.exporter_addr, "name":"hwstate" }, "HardwareState")
         self.swstate_attr = self.addChannel({"type":"exporter", "exporter_address": self.exporter_addr, "name":"swstate" }, "State")
 
@@ -75,13 +74,16 @@ class Microdiff(MiniDiff.MiniDiff):
             if self.phases.has_key(phase):
                 self.movePhase(phase)
                 if wait:
-                    self._wait_ready(40)
+                    if not timeout:
+                        timeout = 40
+                    self._wait_ready(timeout)
     
     def getPhase(self):
         return self.readPhase.getValue()
 
     def moveSyncMotors(self, motors_dict, wait=False, timeout=None):
         argin = ""
+        #print "start moving motors =============", time.time()
         for motor in motors_dict.keys():
             position = motors_dict[motor]
             name=self.MOTOR_TO_EXPORTER_NAME[motor]
@@ -92,43 +94,53 @@ class Microdiff(MiniDiff.MiniDiff):
         if wait:
             while not self._ready():
                 time.sleep(0.5)
-        #self._wait_ready(timeout)
+        #print "end moving motors =============", time.time()
             
     def oscilScan(self, start, end, exptime, wait=False):
         scan_params = "1\t%0.3f\t%0.3f\t%0.4f\t1"% (start, (end-start), exptime)
-        """
-        scan_params = []
-        scan_params.append(int(1))
-        scan_params.append(float(start))
-        scan_params.append(float(end-start))
-        scan_params.append(float(exptime))
-        scan_params.append(int(1)) # only one oscillation)
-        """
         scan = self.addCommand({"type":"exporter", "exporter_address":self.exporter_addr, "name":"start_scan" }, "startScanEx")
         scan(scan_params)
-        print "started at ----------->", time.time()
+        print "scan started at ----------->", time.time()
         if wait:
-            self._wait_ready(1000)
+            self._wait_ready(300) #timeout of 5 min
             print "finished at ---------->", time.time()
 
     def oscilScan4d(self, start, end, exptime,  motors_pos, wait=False):
-        """
-        scan_params = []
-        scan_params.append(start)                 
-        scan_params.append(end - start)
-        scan_params.append(exptime)
-        """
-        scan_params = "%0.3f\t%0.3f\t%0.4f\t"% (start, (end-start), exptime)
-        for i in motors_pos.values():
-            #scan_params.append(i)
-            scan_params += "%0.3f\t", i
-        scan = self.addCommand({"type":"exporter", "exporter_address":self.exporter_addr, "name":"start_scan" }, "startScan4dEx")
+        scan_params = "%0.3f\t%0.3f\t%f\t"% (start, (end-start), exptime)
+        scan_params += "%0.3f\t" % motors_pos['1']['phiy']
+        scan_params += "%0.3f\t" % motors_pos['1']['phiz']
+        scan_params += "%0.3f\t" % motors_pos['1']['sampx']
+        scan_params += "%0.3f\t" % motors_pos['1']['sampy']
+        scan_params += "%0.3f\t" % motors_pos['2']['phiy']
+        scan_params += "%0.3f\t" % motors_pos['2']['phiz']
+        scan_params += "%0.3f\t" % motors_pos['2']['sampx']
+        scan_params += "%0.3f" % motors_pos['2']['sampy']
+
+        scan = self.addCommand({"type":"exporter", "exporter_address":self.exporter_addr, "name":"start_scan4d" }, "startScan4DEx")
         scan(scan_params)
+        print "scan started at ----------->", time.time()
         if wait:
-            self._wait_ready(10000)
+            self._wait_ready(900) #timeout of 15 min
+            print "finished at ---------->", time.time()
 
 def set_light_in(light, light_motor, zoom):
-    MICRODIFF.moveToPhase(phase="Centring",  wait=True, timeout=40)
+    """
+    The move to centring phase can be quite slow, so we use the front
+    light instead.
+    """
+    #MICRODIFF.moveToPhase(phase="Centring",  wait=True, timeout=40)
+    """
+    diffr = MICRODIFF.getDeviceByRole("beamstop")
+    pos = diffr.getActuatorState()
+    if pos == 'in':
+        try:
+            diffr.actuatorOut(wait=True)
+            print "Now put the %s in"% light.username
+            print diffr.getActuatorState()
+            print MICRODIFF._ready()
+            light.actuatorIn(wait=True)
+        except Exception, error:
+            print str(error)
     
     # No light level, choose default
     if light_motor.getPosition() == 0:
@@ -143,8 +155,7 @@ def set_light_in(light, light_motor, zoom):
 
         if light_level:
             light_motor.move(light_level)
+    """
+    MICRODIFF.getDeviceByRole("flight").move(2)
 
 MiniDiff.set_light_in = set_light_in
-
-
-
