66af6eb68b371b077deca7254eb20c738feafd86
bliss administrator <blissadm@cakenew.esrf.fr>
2015-09-10 10:51:53 +0200

    added Adxv notification for new images in BM14MultiCollect.py

--- HardwareObjects/ESRF/BM14MultiCollect.py
+++ HardwareObjects/ESRF/BM14MultiCollect.py
@@ -1,9 +1,12 @@
 from ESRFMultiCollect import *
 from detectors.TacoMar import Mar225
+import os
+import socket
 
 class BM14MultiCollect(ESRFMultiCollect):
     def __init__(self, name):
         ESRFMultiCollect.__init__(self, name, CcdDetector(Mar225), TunableEnergy())
+        self._notify_greenlet = None
 
     @task
     def data_collection_hook(self, data_collect_parameters):
@@ -30,5 +33,37 @@ class BM14MultiCollect(ESRFMultiCollect):
 
     def get_flux(self):
         return 0 
+    
+    @task
+    def set_detector_filenames(self, frame_number, start, filename, jpeg_full_path, jpeg_thumbnail_full_path):
+        self.last_image_filename = filename
+        return ESRFMultiCollect.set_detector_filenames(self, frame_number, start, filename, jpeg_full_path, jpeg_thumbnail_full_path)
   
+    def adxv_notify(self, image_filename):
+        logging.info("adxv_notify %r", image_filename)
+        #image_filename="/data/bm14/inhouse/opd14/20150908/RAW_DATA/adxvtest/opd14adxv3_1_0004.mccd"
+        try:
+            # do a 'stat' to force NFS cache clearing
+            os.stat(os.path.dirname(image_filename))           
+            os.system("ls " + image_filename)
+            if not os.path.isfile(image_filename):
+                logging.info("No image file '%s` on disk (yet)", image_filename)
+            else:
+                adxv_notify_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                adxv_notify_socket.connect(("cakenew.esrf.fr", 8100))
+                adxv_notify_socket.sendall("load_image %s\n" % image_filename)
+                adxv_notify_socket.close()
+        except Exception, err:
+            logging.info("adxv_notify exception : %r", image_filename)
+            #print Exception, err
+            pass
+
+    @task
+    def write_image(self, last_frame):
+        ESRFMultiCollect.write_image(self, last_frame)
+        if last_frame:
+            gevent.spawn_later(1, self.adxv_notify, self.last_image_filename)
+        else:
+            if self._notify_greenlet is None or self._notify_greenlet.ready():
+                self._notify_greenlet = gevent.spawn_later(1, self.adxv_notify, self.last_image_filename)
 
