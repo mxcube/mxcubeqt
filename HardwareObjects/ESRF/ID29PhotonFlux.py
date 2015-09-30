4265866022d31d371ac43595bb86d218a0e48426
beteva@esrf.fr <blissadm@p1-id29new.esrf.fr>
2015-09-30 14:58:15 +0200

    Calculate flux.

---
+++ HardwareObjects/ESRF/ID29PhotonFlux.py
@@ -0,0 +1,84 @@
+from HardwareRepository.BaseHardwareObjects import Equipment
+from HardwareRepository.TaskUtils import *
+import numpy
+import time
+import logging
+import math
+from calc_flux import *
+
+
+class ID29PhotonFlux(Equipment):
+    def __init__(self, *args, **kwargs):
+        Equipment.__init__(self, *args, **kwargs)
+
+    def init(self):
+        self.counter = self.getObjectByRole("counter")
+        self.shutter = self.getDeviceByRole("shutter")
+        self.energy_motor = self.getObjectByRole("energy")
+        self.aperture = self.getObjectByRole("aperture")
+        fname  = self.getProperty("calibrated_diodes_file")
+
+        self.flux_calc = CalculateFlux()
+        self.flux_calc.init(fname)
+        self.shutter.connect("shutterStateChanged", self.shutterStateChanged)
+
+        self.counts_reading_task = self._read_counts_task(wait=False)
+
+    @task
+    def _read_counts_task(self):
+        old_counts = None
+        while True:
+            counts = self._get_counts()
+            if counts != old_counts:
+                old_counts = counts
+                self.countsUpdated(counts)
+            time.sleep(1)
+
+    def _get_counts(self):
+        try:
+            counts = self.counter.getValue()[0]
+            if counts == -9999 :
+                counts = 0
+        except:
+            counts = 0
+            logging.getLogger("HWR").exception("%s: could not get counts", self.name())
+        try:
+          egy = self.energy_motor.getCurrentEnergy()*1000.0
+          calib = self.flux_calc.calc_flux_coef(egy)
+        except:
+          logging.getLogger("HWR").exception("%s: could not get energy", self.name())
+        else:
+            if self.aperture is None:
+                aperture_coef = 1
+            else:
+                try:
+                    aperture_coef = self.aperture.getApertureCoef()
+                except:
+                    aperture_coef = 1.
+            counts = math.fabs(counts * calib[0] * aperture_coef)
+        return counts
+
+    def connectNotify(self, signal):
+        if signal == "valueChanged":
+          self.emitValueChanged()
+
+    def shutterStateChanged(self, _):
+        self.countsUpdated(self._get_counts())
+
+    def updateFlux(self, _):
+        self.countsUpdated(self._get_counts(), ignore_shutter_state=False)
+
+    def countsUpdated(self, counts, ignore_shutter_state=False):
+        self.emitValueChanged("%1.3g" % counts)
+
+    def getCurrentFlux(self):
+        self.updateFlux("dummy")
+        return self.current_flux
+
+    def emitValueChanged(self, flux=None):
+        if flux is None:
+          self.current_flux = None
+          self.emit("valueChanged", ("?", ))
+        else:
+          self.current_flux = flux
+          self.emit("valueChanged", (self.current_flux, ))
---
+++ HardwareObjects/ESRF/calc_flux.py
@@ -0,0 +1,108 @@
+import logging
+import numpy as np
+import scipy.interpolate as sp
+
+class CalculateFlux:
+
+    def __init__(self, fname=None):
+        self.FLUX = {}
+
+    def init(self, fname="/users/blissadm/local/calibrated_diodes.dat"):
+        self.calib_array=self._load_flux_calibration(fname)
+
+    def _load_flux_calibration(self, fname):
+        try:
+            f = open(fname)
+            array = []
+            nb_line = 0
+            for line in f:  
+                if not line.startswith('#') and line.strip() :
+                    array.append(map(float,line.split()))
+                    nb_line += 1
+                else:
+                    if line.startswith('#'):
+                        #line = line[1:]
+                        ll = line[1:].split()
+                        self.labels=[]
+                        idx = []
+                        for i in ll:
+                            self.labels.append(i.lower())
+                            idx.append(0)
+            f.close()
+        except IOError:
+            logging.exception("Cannot read calibrated diodes file")
+        self.FLUX = dict(zip(self.labels, idx))
+
+        return array
+
+    def calc_flux_coef(self, en):
+        if en < 4:
+            logging.exception("Cannot calculate flux - unknown energy")
+            raise ValueError
+        if en < 1000:
+            en *= 1000
+        calib = self._interpol(self.calib_array,en)
+        self.FLUX[self.labels[0]] = en
+        for i in range(calib.__len__()):
+            self.FLUX[self.labels[i+1]] = calib[i]
+        return calib
+
+    def _interpol(self, arr, val, debug=0):
+        larr = []
+        for vals in enumerate(arr):
+            if abs(vals[0] - val)  < 0.01:
+                try:
+                    return [vals[1],vals[2]]
+                except:
+                    return vals[1]
+            larr.append(abs(vals[0] - val))
+        min_index = larr.index(min(larr))
+        x1 = arr[min_index][0]
+        y1 = arr[min_index][1]
+        try:
+            z1 = arr[min_index][2]
+        except:
+            pass
+        if debug:
+            print arr[min_index]
+        if x1 > val and min_index > 0:
+            min_index -= 1
+        elif x1 < val and min_index <  len(arr)-1:
+            min_index += 1
+        elif x1 < val and min_index >= len(arr)-1:
+            min_index -=1
+        else:
+            return None
+        if debug:
+            print arr[min_index]
+        x2 = arr[min_index][0]
+        y2 = arr[min_index][1]
+        try:
+            z2 = arr[min_index][2]
+        except:
+            pass        
+        bb = (y2-y1)/(x2-x1)
+        aa =  y1 - bb*x1
+        try:
+            dd = (z2-z1)/(x2-x1)
+            cc = z1 - dd*x1
+            return [aa+bb*val, cc+dd*val]
+        except:
+            return aa+bb*val
+
+    def _interpol_np(self, arr, val):
+        larr = np.flipud(np.array(arr))
+        y1arr = sp.interp1d(larr[:,0], larr[:,1])
+        try:
+            y2arr = sp.interp1d(larr[:,0], larr[:,2])
+            return [float(y1arr(val)), float(y2arr(val))]
+        except:
+            return float(y1arr(val))
+
+if __name__ == '__main__' :
+    fl = CalculateFlux()
+    fl.init("/users/blissadm/local/spec/userconf/calibrated_diodes.dat")
+    ab = fl.calc_flux_coef(6199.19)
+    print ab
+    ab = fl.calc_flux_coef(6716)
+    print ab
