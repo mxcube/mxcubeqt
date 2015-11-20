a8dc0ac0da2aa4ca552c5b4e9bdfdd499ed20bf8
blissadm@id30_massif3 <blissadm@id30_massif3>
2015-11-20 14:27:18 +0100

    added HardwareObjects as a submodule

--- HardwareObjects/EMBLDataAnalysis.py
+++
@@ -1,8 +0,0 @@
-import DataAnalysis
-from HardwareRepository.BaseHardwareObjects import HardwareObject
-
-class EMBLDataAnalysis(DataAnalysis.DataAnalysis, HardwareObject):
-    #Test
-    def get_beam_size(self):
-        return (0.1,0.2) #(self.execute_command("get_beam_size_x"),
-               # self.execute_command("get_beam_size_y"))
--- HardwareObjects/GrobGonio.py
+++
@@ -1,9 +0,0 @@
-from HardwareRepository.BaseHardwareObjects import Device
-import GrobMotor
-
-class GrobGonio(GrobMotor.GrobMotor):      
-    def __init__(self, name):
-        GrobMotor.GrobMotor.__init__(self, name)
-
-    def oscillation(self, *args):
-        self.motor.oscillation(*args)
