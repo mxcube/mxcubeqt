73f4c40126f9900098532996cf6e046ecdec17ed
IvarsKarpics <ivars.karpics@gmail.com>
2016-07-22 10:54:45 +0200

    2016.07.22. New Qt4_MachineInfoBrick
    Removed static labels and Ledits. When first time set_value method is called it initializes the brick (adds labels and ledits). All values to be displayed on the brick are passed as a list of dict.
    Example of a dictionary:
    
    temp_dict = {}
    temp_dict['value'] = 0
    temp_dict['in_range'] = False
    temp_dict['bold'] = True
    temp_dict['title'] = "Hutch humidity"

--- Bricks/Qt4_MachineInfoBrick.py
+++ Bricks/Qt4_MachineInfoBrick.py
@@ -45,7 +45,8 @@ class Qt4_MachineInfoBrick(BlissWidget):
         self.mach_info_hwobj = None
 
         # Internal values -----------------------------------------------------
-        self.last_value = None
+        self.graphics_initialized = None
+        self.value_label_list = []
 
         # Properties ---------------------------------------------------------- 
         self.addProperty('mnemonic', 'string', '')
@@ -58,61 +59,16 @@ class Qt4_MachineInfoBrick(BlissWidget):
         self.defineSlot('setColDir', ())
          
         # Graphic elements ----------------------------------------------------
-        self.current_label = QtGui.QLabel("Machine current", self)
-        self.current_value_label = QtGui.QLabel(self)
-        self.current_value_label.setAlignment(QtCore.Qt.AlignCenter)
-        bold_font = self.current_value_label.font()
-        bold_font.setPointSize(14)
-        self.current_value_label.setFont(bold_font)
-        #State text
-        self.state_text_label = QtGui.QLabel("Machine state text", self)
-        self.state_text_value_label = QtGui.QLabel(self)
-        self.state_text_value_label.setAlignment(QtCore.Qt.AlignCenter)
-        #Intensity
-        self.intensity_label = QtGui.QLabel("Flux", self)
-        self.intensity_value_label = QtGui.QLabel(self)
-        self.intensity_value_label.setAlignment(QtCore.Qt.AlignCenter)
-	#Hutch temperature
-        self.temperature_label = QtGui.QLabel("Hutch temperature", self)
-        self.temperature_value_label = QtGui.QLabel(self)
-        self.temperature_value_label.setAlignment(QtCore.Qt.AlignCenter)
-        #Hutch humidity
-        self.humidity_label = QtGui.QLabel("Hutch humidity", self)
-        self.humidity_value_label = QtGui.QLabel(self) 
-        self.humidity_value_label.setAlignment(QtCore.Qt.AlignCenter)  
-        #Available disc space
-        self.disc_label = QtGui.QLabel("Storage disc space", self)
-        self.disc_value_label = QtGui.QLabel(self)
-        #Cryostream position
-        self.cryo_label = QtGui.QLabel("Cryojet position", self)
-        self.cryo_value_label = QtGui.QLabel(self)
-        self.cryo_value_label.setFont(bold_font)
-        self.cryo_value_label.setAlignment(QtCore.Qt.AlignCenter)
+
         # Layout --------------------------------------------------------------
-        _main_vlayout = QtGui.QVBoxLayout(self)
-        _main_vlayout.addWidget(self.current_label)
-        _main_vlayout.addWidget(self.current_value_label)
-        _main_vlayout.addWidget(self.state_text_label)
-        _main_vlayout.addWidget(self.state_text_value_label)
-        _main_vlayout.addWidget(self.intensity_label)
-        _main_vlayout.addWidget(self.intensity_value_label)
-        _main_vlayout.addWidget(self.temperature_label)
-        _main_vlayout.addWidget(self.temperature_value_label) 
-        _main_vlayout.addWidget(self.humidity_label)
-        _main_vlayout.addWidget(self.humidity_value_label)
-        _main_vlayout.addWidget(self.disc_label)
-        _main_vlayout.addWidget(self.disc_value_label)
-        _main_vlayout.addWidget(self.cryo_label)
-        _main_vlayout.addWidget(self.cryo_value_label)
-        _main_vlayout.setSpacing(1)
-        _main_vlayout.setContentsMargins(1, 1, 1, 1)
+        self.main_vlayout = QtGui.QVBoxLayout(self)
+        self.main_vlayout.setSpacing(2)
+        self.main_vlayout.setContentsMargins(2, 2, 2, 2)
 
         # SizePolicies --------------------------------------------------------
 
         # Other --------------------------------------------------------------- 
-        self.setToolTip("Main information about current, state, " + \
-                        "intensity, temperature, humidity, storage disc" + \
-                        "and cryo")
+        self.setToolTip("Main information about the beamline")
 
     def propertyChanged(self, property_name, old_value, new_value):
         """
@@ -123,30 +79,60 @@ class Qt4_MachineInfoBrick(BlissWidget):
         if property_name == 'mnemonic':
             if self.mach_info_hwobj is not None:
                 self.disconnect(self.mach_info_hwobj, 'valuesChanged', self.set_value)
-                self.disconnect(self.mach_info_hwobj, 'inRangeChanged', self.set_color)
-                self.disconnect(self.mach_info_hwobj, 'tempHumChanged', self.temp_hum_changed)
-
             self.mach_info_hwobj = self.getHardwareObject(new_value)
             if self.mach_info_hwobj is not None:
                 self.setEnabled(True)
                 self.connect(self.mach_info_hwobj, 'valuesChanged', self.set_value)
-                self.connect(self.mach_info_hwobj, 'inRangeChanged', self.set_color)
-                self.connect(self.mach_info_hwobj, 'tempHumChanged', self.temp_hum_changed)
-                if self.mach_info_hwobj.has_cryo() is False:
-                    self.cryo_label.hide()
-                    self.cryo_value_label.hide()
                 self.mach_info_hwobj.update_values() 
             else:
                 self.setEnabled(False)
         else:
             BlissWidget.propertyChanged(self, property_name, old_value, new_value)
 
-    def set_value(self, values):
+    def set_value(self, values_list):
         """
         Descript. :
         Args.     :
         Return.   : 
         """
+        if not self.graphics_initialized:
+            for item in values_list:
+                temp_label = QtGui.QLabel(item["title"], self)
+                temp_value_label = QtGui.QLabel(self)
+                temp_value_label.setAlignment(QtCore.Qt.AlignCenter)
+                if "bold" in item:
+                    bold_font = temp_value_label.font()
+                    bold_font.setPointSize(14)
+                    temp_value_label.setFont(bold_font)
+                self.main_vlayout.addWidget(temp_label)
+                self.main_vlayout.addWidget(temp_value_label)
+                self.value_label_list.append(temp_value_label)
+
+            self.disc_label = QtGui.QLabel("Storage disc space", self)
+            self.disc_value_label = QtGui.QLabel(self)
+            self.main_vlayout.addWidget(self.disc_label)
+            self.main_vlayout.addWidget(self.disc_value_label)
+            self.graphics_initialized = True
+
+       
+        for index in range(len(values_list)):
+            self.value_label_list[index].setText(str(values_list[index]['value'])) 
+            if values_list[index]['in_range'] is None:
+                 Qt4_widget_colors.set_widget_color(\
+                     self.value_label_list[index], 
+                     STATES['unknown'])
+            elif values_list[index]['in_range']:
+                 Qt4_widget_colors.set_widget_color(\
+                     self.value_label_list[index],
+                     STATES['ready'])
+            else:
+                 Qt4_widget_colors.set_widget_color(\
+                     self.value_label_list[index],
+                     STATES['error'])
+
+
+        return
+
         txt = '??? mA' if values.get("current") is None else '<b>%s</b> mA' % \
 	       str(self['formatString'] % abs(values.get("current")))
         self.current_value_label.setText(txt)
@@ -160,44 +146,6 @@ class Qt4_MachineInfoBrick(BlissWidget):
             self.cryo_value_label.setText("NOT IN PLACE")
         else:
             self.cryo_value_label.setText("Unknown")
-
-    def set_color(self, value):
-        """
-        Descript. :
-        Args.     :
-        Return.   : 
-        """
-        if value.get('current') is None:
-            Qt4_widget_colors.set_widget_color(self.current_value_label, 
-                                               STATES['unknown'])
-        elif value.get('current'):
-            Qt4_widget_colors.set_widget_color(self.current_value_label,
-                                               STATES['ready'])
-        else:
-            Qt4_widget_colors.set_widget_color(self.current_value_label,
-                                               STATES['error'])
-        Qt4_widget_colors.set_widget_color(self.state_text_value_label,
-                                           STATES['ready'])
-        if value.get('flux') is None:
-            Qt4_widget_colors.set_widget_color(self.intensity_value_label,
-                                               STATES['unknown'])
-        elif value.get('flux'):
-            Qt4_widget_colors.set_widget_color(self.intensity_value_label,
-                                               STATES['ready'])
-        else:
-            Qt4_widget_colors.set_widget_color(self.intensity_value_label,
-                                               STATES['error'])
-        self.cryo_value_label.setEnabled(True)
-        if value.get('cryo') is None:
-            self.cryo_value_label.setEnabled(False)
-            Qt4_widget_colors.set_widget_color(self.cryo_value_label,
-                                               STATES['unknown'])
-        elif value.get('cryo'):
-            Qt4_widget_colors.set_widget_color(self.cryo_value_label,
-                                               STATES['ready'])
-        else:
-            Qt4_widget_colors.set_widget_color(self.cryo_value_label,
-                                               STATES['error'])
 
     def sizeof_fmt(self, num):
         """
@@ -251,33 +199,3 @@ class Qt4_MachineInfoBrick(BlissWidget):
             Qt4_widget_colors.set_widget_color(self.disc_value_label,
                                                    STATES['unknown'])
         self.disc_value_label.setText(txt)
-
-    def temp_hum_changed(self, values, valuesInRange):
-        """
-        Descript. :
-        Args.     :
-        Return.   : 
-        """
-        txt = '??? C' if values[0] is None else '%s C' % \
-	      str(self['formatString'] % values[0])
-        self.temperature_value_label.setText(txt)
-        txt = '??? %' if values[1] is None else '%s %%' % \
-	      str(self['formatString'] % values[1])
-        self.humidity_value_label.setText(txt)	           
-        if valuesInRange[0] is None:
-            Qt4_widget_colors.set_widget_color(self.temperature_value_label, 
-                                               STATES['unknown'])
-        elif valuesInRange[0]:
-            Qt4_widget_colors.set_widget_color(self.temperature_value_label, 
-                                               STATES['ready'])
-        else:
-            Qt4_widget_colors.set_widget_color(self.temperature_value_label, 
-                                               STATES['error'])
-        if valuesInRange[1] is None:
-            Qt4_widget_colors.set_widget_color(self.humidity_value_label, 
-                                               STATES['unknown'])
-        elif valuesInRange[1]:
-            Qt4_widget_colors.set_widget_color(self.humidity_value_label, 
-                                               STATES['ready'])
-        else:
-            Qt4_widget_colors.set_widget_color(self.humidity_value_label, STATES['error'])
