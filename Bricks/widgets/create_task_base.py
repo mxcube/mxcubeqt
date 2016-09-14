f664faa58eaad419c0beb1d293e226a2acc44713
bliss administrator <blissadm@pluton.esrf.fr>
2016-09-14 11:10:52 +0200

    Bricks/widgets/create_task_base.py: processing_parameters initialization to None

--- Bricks/widgets/create_task_base.py
+++ Bricks/widgets/create_task_base.py
@@ -44,6 +44,7 @@ class CreateTaskBase(qt.QWidget):
          self._energy_scan_result = None
          self._session_hwobj = None
          self._beamline_setup_hwobj = None
+         self._processing_parameters = None
          
          qt.QObject.connect(qt.qApp, qt.PYSIGNAL('tab_changed'),
                             self.tab_changed)
