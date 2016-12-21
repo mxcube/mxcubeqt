0d94dac2a68251cd15f21f1ba8d8db06cd113fd3
Sicilia Developer Controls <sicilia@bl13mxcube.cells.es>
2016-12-21 17:23:14 +0100

    Small change in MXCUBE_Test_Environment.py

--- MXCuBE_Test_Environment.py
+++ MXCuBE_Test_Environment.py
@@ -304,13 +304,13 @@ class MXCuBE_Test_Environment(object):
             print("Cannot test bricks without Qt")
             sys.exit(0)
 
+        if not self.hwr_server:
+            self._connect_hwserver()
+
         mod = imp.load_source('_brick', filename)
         basename = os.path.basename(filename)
         name, ext = os.path.splitext(basename)
         brick_class = mod.__dict__[name]
-
-        if not self.hwr_server:
-            self._connect_hwserver()
 
         brick = brick_class() 
         return brick, mod
