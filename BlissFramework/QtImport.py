637a34d646099674c1539ca01ebcfe34125e5690
rhfogh <rhfogh@globalphasing.com>
2017-06-02 17:34:50 +0100

    Bug fix - QtImport was incompatible with Qt3

--- BlissFramework/QtImport.py
+++ BlissFramework/QtImport.py
@@ -252,27 +252,29 @@ if qt_imported and mpl_imported:
         print("  !!! Matplotlib is NOT COMPATIBLE with PyQt !!!")
 """
 
-class RotatedHeaderView(QHeaderView):
-    def __init__(self, parent=None):
-        super(RotatedHeaderView, self).__init__(Qt.Horizontal, parent)
-        self.setMinimumSectionSize(22)
+if qt_variant in ('PyQt4', 'PyQt5', 'PySide'):
+    # QHeaderView is not defined for Qt3, so the import broke without the 'if'
+    class RotatedHeaderView(QHeaderView):
+        def __init__(self, parent=None):
+            super(RotatedHeaderView, self).__init__(Qt.Horizontal, parent)
+            self.setMinimumSectionSize(22)
 
-    def paintSection(self, painter, rect, logicalIndex ):
-        painter.save()
-        # translate the painter such that rotate will rotate around the correct point
-        painter.translate(rect.x()+rect.width(), rect.y())
-        painter.rotate(90)
-        # and have parent code paint at this location
-        newrect = QRect(0,0,rect.height(),rect.width())
-        super(RotatedHeaderView, self).paintSection(painter, newrect, logicalIndex)
-        painter.restore()
+        def paintSection(self, painter, rect, logicalIndex ):
+            painter.save()
+            # translate the painter such that rotate will rotate around the correct point
+            painter.translate(rect.x()+rect.width(), rect.y())
+            painter.rotate(90)
+            # and have parent code paint at this location
+            newrect = QRect(0,0,rect.height(),rect.width())
+            super(RotatedHeaderView, self).paintSection(painter, newrect, logicalIndex)
+            painter.restore()
 
-    def minimumSizeHint(self):
-        size = super(RotatedHeaderView, self).minimumSizeHint()
-        size.transpose()
-        return size
+        def minimumSizeHint(self):
+            size = super(RotatedHeaderView, self).minimumSizeHint()
+            size.transpose()
+            return size
 
-    def sectionSizeFromContents(self, logicalIndex):
-        size = super(RotatedHeaderView, self).sectionSizeFromContents(logicalIndex)
-        size.transpose()
-        return size
+        def sectionSizeFromContents(self, logicalIndex):
+            size = super(RotatedHeaderView, self).sectionSizeFromContents(logicalIndex)
+            size.transpose()
+            return size
