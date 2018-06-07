19f93c45408223da89fd0716d01984944131e355
Martin Savko <martin.savko@gmail.com>
2018-06-07 11:55:45 +0200

    Modifications to allow CatsStatusView to inherit from BlissWidget in addition to QGroupBox. Improving log messages in Qt4_CatsSimpleBrick.

--- BlissFramework/Bricks/Qt4_CatsSimpleBrick.py
+++ BlissFramework/Bricks/Qt4_CatsSimpleBrick.py
@@ -37,17 +37,20 @@ from QtImport import *
 
 from Qt4_SampleChangerBrick3 import Qt4_SampleChangerBrick3, BasketView, VialView
 from Qt4_sample_changer_helper import *
+from BlissFramework.Qt4_BaseComponents import BlissWidget
 
 __category__ = "Sample changer"
 
-class CatsStatusView(QGroupBox):
+class CatsStatusView(QGroupBox, BlissWidget):
 
-    def __init__(self, parent):
-        QGroupBox.__init__(self, "Sample Changer State", parent)
-
+    def __init__(self, parent, brick):
+        
+        QGroupBox.__init__(self, "State", parent)
+        BlissWidget.__init__(self, parent)
         # Graphic elements ----------------------------------------------------
         #self.contents_widget = QGroupBox("Sample Changer State", self)
 
+        self._parent = brick
         self.status_label = QLabel("")
         self.status_label.setAlignment(Qt.AlignCenter)
 
@@ -59,13 +62,13 @@ class CatsStatusView(QGroupBox):
         _layout.setContentsMargins(6,6,6,10)
 
     def setStatusMsg(self, status):
-        logging.getLogger("HWR").debug(">>>>> CATS Brick Setting status msg to " + str(status)) 
         self.status_label.setToolTip(status)
         status = status.strip()
         self.setToolTip(status)
 
     def setState(self, state):
-        logging.getLogger("HWR").debug(">>>>> CATS Brick Setting state to " + str(state)) 
+       
+        logging.getLogger("HWR").debug("SC StatusView. State changed %s" % str(state))
         color = SC_STATE_COLOR.get(state, None)
 
         if color is None:
@@ -75,7 +78,9 @@ class CatsStatusView(QGroupBox):
 
         enabled = SC_STATE_GENERAL.get(state, False)
         self.status_label.setEnabled(enabled)
-        self.status_label.setText(SampleChangerState.tostring(state))
+        state_str = SampleChangerState.tostring(state)
+        self.status_label.setText(state_str)
+        self._parent.set_status_info("sc", state_str)
        
     def setIcons(self, *args):
         pass
@@ -143,7 +148,7 @@ class Qt4_CatsSimpleBrick(Qt4_SampleChangerBrick3):
 
 
     def build_status_view(self, container):
-        return CatsStatusView(container)
+        return CatsStatusView(container, self)
 
     def build_operations_widget(self):
         self.buttons_layout = QHBoxLayout()
@@ -188,9 +193,10 @@ class Qt4_CatsSimpleBrick(Qt4_SampleChangerBrick3):
 
         if self.has_basket_HT:
              vials = [[VialView.VIAL_BARCODE]] *10 
-             self.baskets[-1].setMatrices(vials)
+             self.baskets[-1].set_matrices(vials)
 
     def sc_state_changed(self, state, previous_state=None):
+        logging.getLogger("HWR").debug("SC State changed %s" % str(state))
         Qt4_SampleChangerBrick3.sc_state_changed(self, state, previous_state)
 
         self.state = state
@@ -206,11 +212,15 @@ class Qt4_CatsSimpleBrick(Qt4_SampleChangerBrick3):
 
     def _updateButtons(self):
         running = self._pathRunning and True or False
+
         if self.state in [SampleChangerState.Ready, SampleChangerState.StandBy]:
             ready = not running
         else:
             ready = False
+
         poweredOn = self._poweredOn and True or False # handles init state None as False
+
+        logging.getLogger("HWR").debug("updating buttons %s / %s / %s" % (running, poweredOn, self.state))
 
         if not poweredOn:
             self.load_button.setEnabled(False)
@@ -218,6 +228,7 @@ class Qt4_CatsSimpleBrick(Qt4_SampleChangerBrick3):
             self.abort_button.setEnabled(False)
             abort_color = Qt4_widget_colors.LIGHT_GRAY
         elif ready:
+            logging.getLogger("HWR").info("Qt4_CatsSimpleBrick update buttons (ready)")
             self.load_button.setEnabled(True)
             if self.sample_changer_hwobj.hasLoadedSample():
                 self.unload_button.setEnabled(True)
@@ -226,23 +237,20 @@ class Qt4_CatsSimpleBrick(Qt4_SampleChangerBrick3):
             self.abort_button.setEnabled(False)
             abort_color = Qt4_widget_colors.LIGHT_GRAY
         else:
+            logging.getLogger("HWR").info("Qt4_CatsSimpleBrick update buttons (other)")
             self.load_button.setEnabled(False)
             self.unload_button.setEnabled(False)
             self.abort_button.setEnabled(True)
             abort_color = Qt4_widget_colors.LIGHT_RED
 
-        Qt4_widget_colors.set_widget_color(self.abort_button, abort_color)
+        self.abort_button.setStyleSheet("background-color: %s;" % abort_color.name())
+
+        #Qt4_widget_colors.set_widget_color(self.abort_button, abort_color)
 
     def load_selected_sample(self):
 
         basket, vial = self.user_selected_sample
-        logging.getLogger("GUI").info("Loading sample basket: %s / %s" % (basket, vial))
-
-        if self.sample_changer_hwobj.read_diff_phase().upper() != "TRANSFER":
-             _res = QMessageBox.warning(self, "Loading aborted", 
-                 "Diffractometer is not in TRANSFER phase.  Loading sample aborted", 
-                 QMessageBox.Ok)
-             return
+        logging.getLogger("GUI").info("Loading sample: %s / %s" % (basket, vial))
 
         if basket is not None and vial is not None:
             if basket != 100:
@@ -253,7 +261,7 @@ class Qt4_CatsSimpleBrick(Qt4_SampleChangerBrick3):
                 logging.getLogger("GUI").info("Is an HT sample: idx=%s (not implemented yet)" % (vial))
 
     def unload_selected_sample(self):
-        logging.getLogger("GUI").info("Unloading sample basket") 
+        logging.getLogger("GUI").info("Unloading sample") 
         self.sample_changer_hwobj.unload()
 
     def abort_mounting(self):
