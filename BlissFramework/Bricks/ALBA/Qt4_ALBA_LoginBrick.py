4e7b574e4bf0f851b2d433ac8232d935f229938f
Bixente Rey <vrey01@gmail.com>
2018-09-04 15:14:07 +0200

    ALBA LoginBrick
    Adapt ALBA LoginBrick from original ProposalBrick2. Overload do_login_as_proposal method

--- BlissFramework/Bricks/ALBA/Qt4_ALBA_LoginBrick.py
+++ BlissFramework/Bricks/ALBA/Qt4_ALBA_LoginBrick.py
@@ -4,6 +4,11 @@ __category__ = "ALBA"
 from BlissFramework.Utils import Qt4_widget_colors
 from Qt4_ProposalBrick2 import Qt4_ProposalBrick2
 
+from BlissFramework.Qt4_BaseComponents import BlissWidget
+
+import logging
+import time
+
 class Qt4_ALBA_LoginBrick(Qt4_ProposalBrick2):
 
     def __init__(self,*args):
@@ -26,16 +31,17 @@ class Qt4_ALBA_LoginBrick(Qt4_ProposalBrick2):
             prop_password = str(self.proposal_password_ledit.text())
             self.proposal_password_ledit.setText("")
 
-            if prop_type == "" and prop_number == "":
+            if prop_number == "":
                 if self.local_login_hwobj is None:
                     return self.refuseLogin(False,"Local login not configured.")
+
                 try:
                     locallogin_password = self.local_login_hwobj.password
                 except AttributeError:
                     return self.refuseLogin(False,"Local login not configured.")
 
-                if prop_password != locallogin_password:
-                    return self.refuseLogin(None,"Invalid local login password.")
+                if str(prop_password) != str(locallogin_password):
+                    return self.refuseLogin(None,"Invalid local login password")
 
                 now = time.strftime("%Y-%m-%d %H:%M:S")
                 prop_dict = {'code' : '', 'number' : '', 'title' : '', 'proposalId' : ''}
@@ -56,8 +62,38 @@ class Qt4_ALBA_LoginBrick(Qt4_ProposalBrick2):
                 return self.refuseLogin(False,'Not connected to the ISPyB database, unable to get proposal.')
           
 
-            prop_type = 'av'
-            prop_number = '2020000007'
-
             self._do_login_as_proposal(prop_type, prop_number, prop_password, self.lims_hwobj.beamline_name)
+
+    def _do_login_as_proposal(self, proposal_code, proposal_number, proposal_password, beamline_name, impersonate=False):
+        """
+        Descript. :
+        """
+        # Get proposal and sessions
+        proposal = "%s%s" % (proposal_code, proposal_number)
+        logging.getLogger("HWR").debug('ProposalBrick: querying ISPyB database... %s' % proposal)
+
+        prop = self.lims_hwobj.login(proposal, proposal_password)
+        logging.getLogger("HWR").debug('   prop returned by lims is %s' % str(prop))
+
+        if prop['status']['code'] != "ok":
+            msg = prop['status']['msg'] 
+            self.refuseLogin(False, msg)
+            return
+
+        prop = self.lims_hwobj.getProposal(proposal_code, proposal_number)
+
+        logging.getLogger("HWR").debug('   got proposal info from lims: %s' % str(prop))
+        # Check if everything went ok
+        prop_ok = True
+        try:
+            prop_ok = (prop['status']['code'] == 'ok')
+        except KeyError:
+            prop_ok = False
+
+        if not prop_ok:
+            self.ispybDown()
+        else:
+            self.select_proposal(prop)
+            BlissWidget.set_status_info("user", "%s%s@%s" % \
+               (proposal_code, str(proposal_number), beamline_name))
 
