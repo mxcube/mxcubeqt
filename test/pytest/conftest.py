847757fe08abc342e63a1e595287b3855453268c
IvarsKarpics <ivars.karpics@gmail.com>
2018-12-18 13:14:08 +0100

    Added test

---
+++ test/pytest/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+import sys
+import os
+
+TESTS_DIR = os.path.abspath(os.path.dirname(__file__))
+MXCUBE_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+HWR_XML_FILES = os.path.join(MXCUBE_ROOT, "HardwareRepository/configuration/xml-qt")
+
+sys.path.insert(0, MXCUBE_ROOT)
+
+print ("MXCuBE Qt root: %s" % MXCUBE_ROOT)
+print ("Config path: %s" % HWR_XML_FILES)
+
+
+@pytest.fixture(scope="session")
+def mxcube_root():
+    return MXCUBE_ROOT
+"""
+from HardwareRepository.HardwareRepository import getHardwareRepository
+
+@pytest.fixture(scope="session")
+def hwr():
+    hwr = getHardwareRepository(HWR_XML_FILES)
+    hwr.connect()
+    return hwr
+
+@pytest.fixture(scope="session")
+def blsetup(hwr):
+    return hwr.getHardwareObject("beamline-setup")
+"""
---
+++ test/pytest/test_mxcube_qt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+import os
+import pytest
+
+import QtImport
+
+def test_mxcube_qt(mxcube_root):
+    sys.path.insert(0, mxcube_root)
+    gui_file = os.path.join(mxcube_root,
+                            "ExampleFiles/example_mxcube_qt4.yml")
+    hr_server = os.path.join(mxcube_root,
+                             "HardwareRepository/configuration/xml-qt")
+    os.environ['HARDWARE_REPOSITORY_SERVER'] = hr_server
+    from BlissFramework import Qt4_startGUI
+ 
+    print("GUI file: %s" % gui_file)
+    Qt4_startGUI.run(gui_file)
+
+    for widget in QtImport.getQApp().allWidgets():
+        if hasattr(widget, "test_pytest"):
+            widget.test_pytest()
