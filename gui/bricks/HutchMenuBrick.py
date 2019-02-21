bafa2ab10e0a6e5ec1a3be4543b91c3d2ec86dd9
IvarsKarpics <ivars.karpics@gmail.com>
2019-02-21 22:00:56 +0100

    Added config file for pylint

--- gui/bricks/HutchMenuBrick.py
+++ gui/bricks/HutchMenuBrick.py
@@ -17,7 +17,6 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 import logging
 
 import api
