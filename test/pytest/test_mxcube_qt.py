cd7c7a3469f309d3fd7d873af22072e76582ddee
IvarsKarpics <ivars.karpics@gmail.com>
2021-03-10 14:37:16 +0100

    Fix travis tests

--- test/pytest/test_mxcube_qt.py
+++ test/pytest/test_mxcube_qt.py
@@ -4,17 +4,13 @@ import os
 import gevent
 
 MXCUBE_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
-
-print("MXCuBE root: %s" % MXCUBE_ROOT)
-
-#sys.path.insert(0, MXCUBE_ROOT)
 MXCUBE_GUI_FILE = os.path.join(MXCUBE_ROOT,
                                "mxcubeqt/example_config.yml")
 def close_app():
     sys.exit(0)
 
-gevent.spawn_later(10, close_app)
 
 from mxcubeqt import create_app
-print("MXCuBE gui file: %s" % MXCUBE_GUI_FILE)
+
+gevent.spawn_later(10, close_app)
 app = create_app(MXCUBE_GUI_FILE)
