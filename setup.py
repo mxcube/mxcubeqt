7c278f5888a52ef92550d185ee5b6855d89bcdf0
Jordi Andreu <jandreu@cells.es>
2021-06-02 09:45:45 +0200

    Add setup.py file
    Adds setup.py file and add missing requirement.

--- setup.py
+++ setup.py
@@ -4,25 +4,23 @@ import sys
 from setuptools import setup, find_packages
 
 
-requirements = ['setuptools']
+if sys.version_info >= (3, 0):
+    requirements_filename = 'requirements_python3.txt'
+else:
+    requirements_filename = 'requirements_python2.txt'
+
+with open(requirements_filename) as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+requirements = [x for x in requirements if not x.startswith('#')]
 
 setup_requirements = []
 
-TESTING = any(x in sys.argv for x in ['test', 'pytest'])
-if TESTING:
-    setup_requirements += ['pytest-runner']
+tests_requirements = []
 
-SPHINX = any(x in sys.argv for x in ['build_sphinx'])
-if SPHINX:
-    setup_requirements += ['sphinx', 'sphinx-argparse', 'sphinx_rtd_theme']
+extras_requirements = {'pyqt5': ['PyQt5==5.14']}
 
-tests_requirements = []  # ['pytest', 'pytest-cov']
-
-extras_requirements = {} # {
-    # 'gui': ['PyQt4', 'taurus', 'taurus-pyqtgraph', 'sardana', 'matplotlib']
-#}
-
-console_scripts = ['mxcube = mxcubeqt:run']
+console_scripts = ['mxcube = mxcubeqt.__main__:run']
 
 gui_scripts = []
 
@@ -31,15 +29,13 @@ entry_points = {
         'gui_scripts': gui_scripts
     }
 
-
 setup(
     name='mxcubeqt',
-    # The version is updated automatically with bumpversion
-    version='0.0.1',
-    #author='Jordi Andreu',
-    #author_email='jandreu@cells.es',
+    # version='0.1.0',
+    # author='The MXCuBE developers',
+    # author_email='mxcube@mxcube.org',
     description='Qt front-end version for MXCuBE',
-    long_description='Qt front-end version for MXCuBE',
+    long_description='Front-end version for MXCuBE based on Qt framework',
     url='http://github.com/mxcube/mxcube',
     packages=find_packages(),
     package_dir={},
@@ -47,21 +43,19 @@ setup(
     package_data={'mxcubeqt': ['ui_files/*.ui',
                                'icons/*.png',
                                'examples/*'],
-                  'mxcubecore': ['configuration/mockup/*.xml',
-                                         'configuration/mockup/*.jpg',
-                                         'configuration/mockup/qt/*.xml',
-                                         'configuration/mockup/qt/*.yml']
                   },
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: Lesser General Public License v3 (LGPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
     ],
     platforms='all',
-    license='GPL-3.0+',
+    license='LGPL',
     entry_points=entry_points,
     install_requires=requirements,
     setup_requires=setup_requirements,
