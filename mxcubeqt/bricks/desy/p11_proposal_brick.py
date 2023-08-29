8e109dc41258fa5f75d6b4b509e608d83659854c
Andrey Gruzinov <andrey.gruzinov@desy.de>
2023-08-29 15:45:56 +0200

    Code cleanup

--- mxcubeqt/bricks/desy/p11_proposal_brick.py
+++ mxcubeqt/bricks/desy/p11_proposal_brick.py
@@ -1,10 +1,9 @@
-
-
 from mxcubeqt.utils import colors, icons, qt_import
 from mxcubeqt.bricks.proposal_brick import ProposalBrick, ProposalGUIEvent
 from mxcubecore import HardwareRepository as HWR
 import logging
 import os
+
 
 class P11ProposalBrick(ProposalBrick):
     def __init__(self, *args):
@@ -26,7 +25,7 @@ class P11ProposalBrick(ProposalBrick):
         proposal_code = HWR.beamline.session.get_current_proposal_code()
 
         if not HWR.beamline.lims or proposal_code is None:
-            self.message_widget.setText('not connected')
+            self.message_widget.setText("not connected")
             self.message_widget.show()
             self.login_as_proposal_widget.hide()
             self.login_button.hide()
@@ -39,7 +38,7 @@ class P11ProposalBrick(ProposalBrick):
             HWR.beamline.session.proposal_number = ""
 
             self.setWindowTitle.emit(self["titlePrefix"])
-            self.proposal_number_ledit.setText('no ispyb')
+            self.proposal_number_ledit.setText("no ispyb")
 
             # self.loggedIn.emit(False)
             # self.sessionSelected.emit(None, None, None, None, None, None, None)
@@ -80,20 +79,19 @@ class P11ProposalBrick(ProposalBrick):
             proposal_number = HWR.beamline.session.get_current_proposal_number()
 
         logging.getLogger("HWR").debug(" PROPOSAL BRICK - code is %s" % proposal_code)
-        logging.getLogger("HWR").debug(" PROPOSAL BRICK - number is %s" % proposal_number)
+        logging.getLogger("HWR").debug(
+            " PROPOSAL BRICK - number is %s" % proposal_number
+        )
 
         self._do_login_as_proposal(
-                proposal_code,
-                proposal_number,
-                None,
-                HWR.beamline.lims.beamline_name,
+            proposal_code, proposal_number, None, HWR.beamline.lims.beamline_name,
         )
 
     def show_selected_proposal(self, proposal):
 
         beamtime_id = HWR.beamline.session.get_current_beamtime_id()
-        prop_number = str(proposal['number'])
-        prop_code = str(proposal['code'])
+        prop_number = str(proposal["number"])
+        prop_code = str(proposal["code"])
 
         prop_info = f"ID: {prop_code}-{prop_number} - BT_ID: {beamtime_id}"
 
@@ -104,6 +102,4 @@ class P11ProposalBrick(ProposalBrick):
         self.proposal_type_combox.hide()
         self.proposal_number_ledit.hide()
         self.password_label.hide()
-        self.proposal_password_ledit.hide() 
-
-
+        self.proposal_password_ledit.hide()
