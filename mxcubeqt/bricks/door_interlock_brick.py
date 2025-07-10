475f0d35b147314fc21a0fc77454c7f382dee0cc
rhfogh <rhfogh@zoho.eu>
2025-07-10 16:21:38 +0100

    Bug fixes to allow qt mock execution with yaml config files

--- mxcubeqt/bricks/door_interlock_brick.py
+++ mxcubeqt/bricks/door_interlock_brick.py
@@ -84,11 +84,17 @@ class DoorInterlockBrick(BaseWidget):
         self.state_label.setToolTip("Shows the current door state")
         self.unlock_door_button.setToolTip("Unlocks the doors")
 
-        self.connect(HWR.beamline.hutch_interlock, "valueChanged", self.value_changed)
-
+        if HWR.beamline.hutch_interlock is not None:
+            self.connect(
+                HWR.beamline.hutch_interlock,
+                "valueChanged",
+                self.value_changed
+            )
+        
     def unlock_doors(self):
         self.unlock_door_button.setEnabled(False)
-        HWR.beamline.hutch_interlock.unlock()
+        if HWR.beamline.hutch_interlock is not None:
+            HWR.beamline.hutch_interlock.unlock()
 
     def updateLabel(self, label):
         self.main_groupbox.setTitle(label)
