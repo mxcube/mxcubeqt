ab8866be0b4ba8f2a96531158d0f2e13ff87c00a
rhfogh <rhfogh@zoho.eu>
2025-07-29 14:59:02 +0100

    Fixed machine_info_brick bug in Andrey Gruzinov 2024097 commit

--- mxcubeqt/bricks/machine_info_brick.py
+++ mxcubeqt/bricks/machine_info_brick.py
@@ -169,3 +169,4 @@ class CustomInfoWidget(qt_import.QWidget):
             self.value_plot.add_new_plot_value(value)
 
     def open_history_view(self):
+        self.value_plot.setVisible(not self.value_plot.isVisible())
